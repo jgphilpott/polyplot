78e4d18ea829036ac463b58b3aa65969a5b0dd24
jgphilpott <jacob.gary.philpott@gmail.com>
2017-02-11 19:34:34 +0300

    Swapped Sqlite3 for Postgres as the Active Record Database

--- iGraph/db/schema.rb
+++ iGraph/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170211152915) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "indices", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
