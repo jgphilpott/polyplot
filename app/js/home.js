6121797faafa7458f21fe57ccd1c3ca0f05129bb
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-19 13:23:23 -0500

    first real data ploted

--- app/js/home.js
+++ app/js/home.js
@@ -15,9 +15,9 @@ $(document).ready(function() {
   var near = 1
   var far = 1000
   var camera = new THREE.PerspectiveCamera(fov, width/height, near, far)
-  camera.position.x = 200
-  camera.position.y = 200
-  camera.position.z = 200
+  camera.position.x = 100
+  camera.position.y = 100
+  camera.position.z = 100
 
   var controls = new THREE.OrbitControls(camera, canvas)
 
@@ -44,6 +44,27 @@ $(document).ready(function() {
   scene.add(x_line)
   scene.add(y_line)
   scene.add(z_line)
+
+  var geometry = new THREE.SphereGeometry( 3, 32, 32);
+  var material = new THREE.MeshStandardMaterial( {color: 0x0000ff} );
+
+  for (var i = 0; i < xData.length; i++) {
+
+    var sphere = new THREE.Mesh( geometry, material )
+
+    if (typeof xData[i]["2000"] == "number") {
+        // console.log(xData[i]["2000"])
+        sphere.position.x = xData[i]["2000"]
+        sphere.position.y = 10
+        sphere.position.z = 10
+        scene.add( sphere )
+    }
+
+  }
+
+  var light = new THREE.PointLight(0xffffff, 3);
+  light.position.set(-100,200,100);
+  scene.add(light);
 
   function animate() {
 
--- app/root.py
+++ app/root.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
 from sass import compile
 
+from json import load
+from os import getcwd
+
+with open(getcwd() + "/app/db/fertility.json") as json_file:
+    fertility = load(json_file)
+
+with open(getcwd() + "/app/db/lifeExpectancy.json") as json_file:
+    lifeExpectancy = load(json_file)
+
+with open(getcwd() + "/app/db/population.json") as json_file:
+    population = load(json_file)
+
 app = Flask("iGraph", template_folder="app", static_folder="app")
 
 @app.route("/")
 def home():
-    return render_template("html/home.html")
+    return render_template("html/home.html", myData=fertility)
 
 if __name__ == "__main__":
     compile(dirname=("app/sass", "app/css"))
