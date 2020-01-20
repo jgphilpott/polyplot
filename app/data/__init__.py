c6ff63518f7bb4e03f385978723c0c681388a943
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-20 18:05:00 -0500

    re-organizing data

--- app/data/__init__.py
+++
@@ -1,9 +0,0 @@
-from data.collections import init_db
-from pymongo import MongoClient
-from os import environ
-
-client = MongoClient(environ["MONGO_PORT_27017_TCP_ADDR"], 27017)
-
-iGraph = client.iGraph
-
-init_db(iGraph)
