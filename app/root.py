9f2367842e28abf3c81c98d739d4db2a7fc1e3df
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-19 23:21:08 -0500

    root update

--- app/root.py
+++ app/root.py
@@ -1,23 +1,12 @@
 from flask import Flask, render_template
 from sass import compile
-
-from json import load
-from os import getcwd
-
-with open(getcwd() + "/app/db/fertility.json") as json_file:
-    fertility = load(json_file)
-
-with open(getcwd() + "/app/db/lifeExpectancy.json") as json_file:
-    lifeExpectancy = load(json_file)
-
-with open(getcwd() + "/app/db/population.json") as json_file:
-    population = load(json_file)
+import data
 
 app = Flask("iGraph", template_folder="app", static_folder="app")
 
 @app.route("/")
 def home():
-    return render_template("html/home.html", myData=fertility)
+    return render_template("html/home.html")
 
 if __name__ == "__main__":
     compile(dirname=("app/sass", "app/css"))
