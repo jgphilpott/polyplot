9bf55c8e40bcb0e2fb530981bc2fc12a0b1f2016
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-21 15:53:22 -0500

    indicators update

--- app/mongo/indicator.py
+++ app/mongo/indicator.py
@@ -1,31 +1,31 @@
 from os import getcwd
 from json import load
 
-from pprint import pprint as pp
-
 def get_indicator(code, log=False):
 
     indicators = get_indicators()
-    indicator = [obj for obj in indicators if obj["code"] in [code]]
+    indicator = [obj for obj in indicators if obj["code"] in [str(code)]]
 
     if indicator:
 
         if log:
 
-            print("\n\033[93mFound indicator:\033[0m {}\n".format(indicator[0]["code"]))
-            pp(indicator[0])
-            print()
+            print("\n\033[93mFound indicator:\033[0m {}\n\n{}\n".format(str(indicator[0]["code"]), str(indicator[0])))
 
         else:
 
             return indicator[0]
 
+    else:
+
+        print("\n\033[91mIndicator\033[0m {} \033[91mnot found.\033[0m\n".format(str(code)))
 
 def get_indicators(log=False):
 
     cwd = getcwd() + "/app/mongo"
 
     with open(cwd + "/data/indicators.json") as list:
+
         indicators = load(list)
 
     if log:
@@ -36,8 +36,7 @@ def get_indicators(log=False):
         for indicator in indicators:
 
             count += 1
-            print("\n\033[93mIndicator #{}:\033[0m".format(count))
-            pp(indicator)
+            print("\n\033[93mIndicator #{}:\033[0m\n{}".format(str(count), str(indicator)))
 
         print()
 
