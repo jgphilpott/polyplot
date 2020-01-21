f9dd71718ec77c8b066241a52cf89d83fcc3fde8
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-21 03:20:21 -0500

    collections update

--- app/mongo/collection.py
+++ app/mongo/collection.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from requests import get
 
 from app.mongo.database import get_database
@@ -5,20 +6,52 @@ from app.mongo.indicator import get_indicators
 
 database = get_database()
 
-def get_collection():
-    pass
+def get_collection(collection, log=False):
 
-def get_collections():
-    return database.list_collection_names()
+    if log:
 
-def collect_indicator(collection, indicator):
+        print("\n\033[93mCollection: \033[0m{}\n\n{}\n".format((str(collection)), database[str(collection)]))
 
-    api = "http://api.worldbank.org/v2/country/all/indicator/{}?format=json".format(indicator["code"])
+    else:
+
+        return database[str(collection)]
+
+def get_collections(log=False):
+
+    if log:
+
+        collections = database.list_collection_names()
+        total = len(collections)
+        print("\n\033[93mThere are a total of {} collections.\033[0m\n".format(total))
+        count = 0
+
+        for collection in collections:
+
+            count += 1
+            print("\033[93mCollection #{}:\033[0m {}".format(count, str(collection)))
+
+        print()
+
+    else:
+
+        return database.list_collection_names()
+
+def collect_indicator(indicator, log=False):
+
+    collection = database[indicator["code"]]
+
+    api = "https://api.worldbank.org/v2/country/all/indicator/{}?format=json".format(indicator["code"])
     meta = get(api).json()
 
     page = 1
     pages = meta[0]["pages"]
+
     items = []
+
+    if log:
+
+        print("\033[93mCollecting:\033[0m {}".format(indicator["code"]))
+        bar = tqdm(initial=page, total=pages)
 
     while page <= pages:
 
@@ -27,27 +60,77 @@ def collect_indicator(collection, indicator):
 
         for item in data:
 
-            region_exists = [region for region in items if region["code"] in [item["countryiso3code"]]]
+            obj_exists = [obj for obj in items if obj["code"] in [item["countryiso3code"]]]
             obj = {"year": int(item["date"]), "value": item["value"]}
 
-            if region_exists:
-                region_exists[0]["history"].append(obj)
+            if obj_exists:
+                obj_exists[0]["history"].append(obj)
             else:
                 items.append({"code": item["countryiso3code"], "history": [obj]})
 
+        bar.update(1)
         page += 1
 
     collection.insert_many(items)
 
-def collect_indicators():
+def collect_indicators(log=False):
 
     collections = get_collections()
     indicators = get_indicators()
 
+    if log:
+
+        print("\n\033[93mThere are a total of {} indicators.\033[0m\n".format(len(indicators)))
+
     for indicator in indicators:
+
         if indicator["code"] not in collections:
+
             try:
-                collection = database[indicator["code"]]
-                collect_indicator(collection, indicator)
+
+                if log:
+
+                    collect_indicator(indicator, log=True)
+
+                else:
+
+                    collect_indicator(indicator)
+
             except:
-                pass
+
+                if log:
+
+                    print("\033[91mError collecting indicator: \033[0m{}".format(indicator["code"]))
+
+        else:
+
+            if log:
+
+                print("\033[93mIndicator\033[0m {} \033[93malready exists.\033[0m".format(indicator["code"]))
+
+    print()
+
+def drop_collection(collection, log=False):
+
+    if log:
+
+        print("\n\033[91mWarning! Are you sure you want to drop the collection:\033[0m {}\n".format(str(collection)))
+
+        while True:
+
+            reply = str(input("\033[93my or n:\033[0m ")).lower()
+
+            if reply == "y":
+
+                get_collection(str(collection)).drop()
+                print("\n\033[93mOkay, the collection\033[0m {} \033[93mhas been droped!\033[0m\n".format(str(collection)))
+                break
+
+            elif reply == "n":
+
+                print()
+                break
+
+    else:
+
+        return get_collection(str(collection)).drop()
