d3a6f71bbf9a666c885ef5aa0867d2cff10f81e9
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-22 01:34:53 -0500

    new routes added

--- app/routes/blueprints.py
+++ app/routes/blueprints.py
@@ -5,4 +5,14 @@ def register_routes(app):
     @app.route("/")
     def home():
 
-        return render_template("html/home.html", myData=[])
+        return render_template("html/home.html")
+
+    @app.route("/2D_scatter")
+    def _2D_scatter():
+
+        return render_template("html/2D_scatter.html")
+
+    @app.route("/3D_scatter")
+    def _3D_scatter():
+
+        return render_template("html/3D_scatter.html")
