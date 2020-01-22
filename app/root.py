f5c847ed6d4ae10d79b192a30bdebab55f16f76b
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-22 01:01:16 -0500

    root update

--- app/root.py
+++ app/root.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template
-from sass import compile
+from flask import Flask
+from tools.setup import config
+from flask_socketio import SocketIO
 
 app = Flask("iGraph", template_folder="app", static_folder="app")
 
-@app.route("/")
-def home():
-    return render_template("html/home.html")
-
 if __name__ == "__main__":
-    compile(dirname=("app/sass", "app/css"))
-    app.jinja_env.auto_reload = True
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+    config(app)
+    socketio = SocketIO(app)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
