a7ce8bbe5dc0b8d4b4a5d1cfc8c7c4fee2ff076c
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-24 15:16:53 -0500

    downloader update

--- app/back/tools/downloader/libs.py
+++ app/back/tools/downloader/libs.py
@@ -1,17 +1,22 @@
 from urllib.request import urlretrieve
 
-def get_libs():
+def get_js_libs(js_libs_dir):
 
-    libs_dir = "app/front/libs/vendor"
+    urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js", "{}/jquery.js".format(js_libs_dir))
+
+    urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/d3/5.15.0/d3.min.js", "{}/d3.js".format(js_libs_dir))
+
+    urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js", "{}/three.js".format(js_libs_dir))
+    urlretrieve("https://threejs.org/examples/js/controls/OrbitControls.js", "{}/orbitControls.js".format(js_libs_dir))
+
+def get_libs(app_folder):
+
+    libs_dir = app_folder + "/libs/vendor"
+    js_libs_dir = libs_dir + "/js"
 
     try:
 
-        urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js", "{}/jquery.js".format(libs_dir))
-
-        urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/d3/5.15.0/d3.min.js", "{}/d3.js".format(libs_dir))
-
-        urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js", "{}/three.js".format(libs_dir))
-        urlretrieve("https://threejs.org/examples/js/controls/OrbitControls.js", "{}/orbitControls.js".format(libs_dir))
+        get_js_libs(js_libs_dir)
 
     except:
 
