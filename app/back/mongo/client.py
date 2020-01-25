0a78a9b0ac81f0bffe83ba5a2a3dba852566bffe
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-25 00:18:06 -0500

    client update

--- app/back/mongo/client.py
+++ app/back/mongo/client.py
@@ -1,15 +1,13 @@
-from pymongo import MongoClient
 from os import environ
+from pymongo import MongoClient
 
 client = MongoClient(environ["MONGO_PORT_27017_TCP_ADDR"], 27017)
 
-database = client.iGraph
-
-def get_client(log=False):
+def find_client(log=False):
 
     if log:
 
-        print("\n\033[93mThe mongo client:\033[0m\n\n{}\n".format(str(client)))
+        print("\n\033[93mThe mongo client:\033[0m\n\n{}\n".format(client))
 
     else:
 
