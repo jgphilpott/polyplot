80bc5f5f2238f707a0f7673ae89513997d9d5238
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-25 20:01:32 -0500

    countries update

---
+++ app/back/mongo/data/collect/countries/mongo.py
@@ -0,0 +1,19 @@
+from back.mongo.data.collect.ions import find_collection
+
+countries = find_collection("countries")
+
+def find_country():
+
+    pass
+
+def find_countries():
+
+    pass
+
+def update_country():
+
+    pass
+
+def update_countries():
+
+    pass
