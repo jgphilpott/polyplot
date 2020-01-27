e4c8b9dac36fb7bdedc4491cdcae7d62eb8fd89a
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-26 20:39:42 -0500

    updating three route

--- app/front/tree/home/three/route.py
+++ app/front/tree/home/three/route.py
@@ -1,10 +1,17 @@
 from flask import render_template
+from back.tools.geometries.sphere import spherify
 
 def register_three_route(app):
 
     @app.route("/three")
     def three():
 
-        data = None
+        try:
+
+            data = spherify()
+
+        except:
+
+            data = None
 
         return render_template("tree/home/three/page.html", data=data)
