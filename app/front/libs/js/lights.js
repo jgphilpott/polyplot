411197324283029b66c1badc9dd784c3955f5c18
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-01-27 17:33:10 -0500

    materials and lights update

--- app/front/libs/js/lights.js
+++ app/front/libs/js/lights.js
@@ -1,4 +1,4 @@
-function new_light(scene, color=white, intensity=1, x=100, y=100, z=100) {
+function new_light(scene, color=white, intensity=3, x=100, y=100, z=100) {
 
   light = new THREE.PointLight(color, intensity)
   light.position.set(x, y, z)
--- app/front/libs/js/materials.js
+++ app/front/libs/js/materials.js
@@ -3,3 +3,9 @@ function new_line_material(color=black, width=1) {
   return new THREE.LineBasicMaterial({color: color, linewidth: width})
 
 }
+
+function new_mesh_material(color=red) {
+
+  return new THREE.MeshStandardMaterial({color: color})
+
+}
