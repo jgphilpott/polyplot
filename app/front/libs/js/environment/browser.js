ae4df7f9b1e57db0606ece732d87b6ea7b8f9cd5
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-02-09 22:50:01 -0500

    touch up

--- app/front/libs/js/environment/browser.js
+++ app/front/libs/js/environment/browser.js
@@ -3,7 +3,7 @@ function findBrowser() {
   // Credit: https://stackoverflow.com/a/9851769/1544937
   firefox = typeof InstallTrigger !== "undefined"
   chrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)
-  safari = /constructor/i.test(window.HTMLElement) || (function (p) {return p.toString() === "[object SafariRemoteNotification]"})(!window["safari"] || (typeof safari !== "undefined" && safari.pushNotification))
+  safari = /constructor/i.test(window.HTMLElement) || (function(p) {return p.toString() === "[object SafariRemoteNotification]"})(!window["safari"] || (typeof safari !== "undefined" && safari.pushNotification))
   ie = false || !!document.documentMode
   edge = !ie && !!window.StyleMedia
   opera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(" OPR/") >= 0
@@ -18,9 +18,13 @@ function findBrowser() {
               {"name": "blink", "status": blink}]
 
   for (var i = 0; i < browsers.length; i++) {
+
     if (browsers[i]["status"]) {
+
       return browsers[i]["name"]
+
     }
+
   }
 
 }
