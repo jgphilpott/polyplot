d38bee18898aecb384d01f3a5a257c096fa106d8
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-02-18 20:07:00 -0500

    working on animation

--- app/front/tree/home/poly3/script.mjs
+++ app/front/tree/home/poly3/script.mjs
@@ -1,15 +1,9 @@
-import {year} from "../../../libs/mjs/time/years.mjs"
-
-import {rainbow} from "../../../libs/mjs/colors/three/rainbow.mjs"
-import {grayscale} from "../../../libs/mjs/colors/three/grayscale.mjs"
-import {regionsColourSwitch} from "../../../libs/mjs/colors/switches/regions.mjs"
-
-import {min, max, width, height} from "../../../libs/mjs/env/dimensions.mjs"
+import {width, height} from "../../../libs/mjs/env/dimensions.mjs"
 
 import {newRenderer} from "../../../libs/mjs/threeCore/renderer.mjs"
 import {newCanvas} from "../../../libs/mjs/threeCore/canvas.mjs"
 import {newScene} from "../../../libs/mjs/threeCore/scene.mjs"
-import {xEvents, xEvent} from "../../../libs/mjs/threeCore/events.mjs"
+import {xEvent, xEvents} from "../../../libs/mjs/threeCore/events.mjs"
 
 import {addAmbientLight} from "../../../libs/mjs/lights/ambient.mjs"
 import {addPointLight} from "../../../libs/mjs/lights/point.mjs"
@@ -18,63 +12,76 @@ import {newPerspectiveCamera} from "../../../libs/mjs/cameras/perspective.mjs"
 
 import {newOrbitControls} from "../../../libs/mjs/controls/orbit.mjs"
 
-import {rangeAxis, absMaxValue} from "../../../libs/mjs/scales/range.mjs"
-import {linearScale} from "../../../libs/mjs/scales/linear.mjs"
-
 import {addAxes} from "../../../libs/mjs/geometries/axes.mjs"
 import {newSphere} from "../../../libs/mjs/geometries/sphere.mjs"
 
+import {rScale, xScale, yScale, zScale} from "../../../libs/mjs/scales/axes.mjs"
+
+import {regionsColourSwitch} from "../../../libs/mjs/colors/switches/regions.mjs"
+
 import {metaUpdate, metaClear} from "../../../libs/mjs/panels/meta.mjs"
+
+import {animatePlots} from "../../../libs/mjs/animation/plots.mjs"
 
 $(document).ready(function() {
 
   // Setup
-  let renderer = newRenderer(width, height)
-  let canvas = newCanvas(renderer)
-  let scene = newScene()
+  data.plot.renderer = newRenderer(width, height)
+  data.plot.canvas = newCanvas(data.plot.renderer)
+  data.plot.scene = newScene()
 
   // Lights
-  addAmbientLight(scene)
-  addPointLight(scene)
+  addAmbientLight(data.plot.scene)
+  addPointLight(data.plot.scene)
 
   // Camera
   let camera = newPerspectiveCamera(width, height)
 
   // Action
-  let controls = newOrbitControls(camera, canvas)
-  let dom	= xEvents(camera, renderer)
+  let controls = newOrbitControls(camera, data.plot.canvas)
+  let dom	= xEvents(camera, data.plot.renderer)
 
-  let rMax = absMaxValue(rangeAxis("r"))
-  let xMax = absMaxValue(rangeAxis("x"))
-  let yMax = absMaxValue(rangeAxis("y"))
-  let zMax = absMaxValue(rangeAxis("z"))
-
-  addAxes(scene)
+  addAxes(data.plot.scene)
 
   let plots = data.plot.plots
 
   for (let i = 0; i < plots.length; i++) {
 
-    let r = plots[i]["r"].find(item => item.year == year)["value"]
-    let x = plots[i]["x"].find(item => item.year == year)["value"]
-    let y = plots[i]["y"].find(item => item.year == year)["value"]
-    let z = plots[i]["z"].find(item => item.year == year)["value"]
+    let r = plots[i]["r"].find(item => item.year == data.plot.year).value
+    let x = plots[i]["x"].find(item => item.year == data.plot.year).value
+    let y = plots[i]["y"].find(item => item.year == data.plot.year).value
+    let z = plots[i]["z"].find(item => item.year == data.plot.year).value
 
     if (r != null && x != null && y != null && z != null) {
 
-      r = linearScale(r, [min, rMax], [max / 150, max / 15])
-      x = linearScale(x, [-xMax, xMax], [-max, max])
-      y = linearScale(y, [-yMax, yMax], [-max, max])
-      z = linearScale(z, [-zMax, zMax], [-max, max])
+      r = rScale(r)
+      x = xScale(x)
+      y = yScale(y)
+      z = zScale(z)
 
       let sphere = newSphere(r, x, y, z, regionsColourSwitch(plots[i]["region"]))
       xEvent(dom, sphere, "mouseover", metaUpdate, plots[i]["code"])
       xEvent(dom, sphere, "mouseout", metaClear)
-      scene.add(sphere)
+      plots[i]["object"] = sphere
+      data.plot.scene.add(sphere)
+
+    } else {
+
+      plots[i]["object"] = null
 
     }
 
   }
+
+  $("body").on("keypress", function(event) {
+
+    if (event.keyCode == 32) {
+
+      animatePlots(dom)
+
+    }
+
+  })
 
   function animate() {
 
@@ -82,7 +89,7 @@ $(document).ready(function() {
 
     controls.update()
 
-  	renderer.render(scene, camera)
+  	data.plot.renderer.render(data.plot.scene, camera)
 
   }
 
