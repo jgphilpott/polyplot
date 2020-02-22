5be2edae0a201e4708c2e00cf6eaf675a4bccff5
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-02-21 21:52:53 -0500

    events + plot update

--- app/front/libs/mjs/geometries/plot.mjs
+++ app/front/libs/mjs/geometries/plot.mjs
@@ -1,14 +1,21 @@
 import {black} from "../colors/three/grayscale.mjs"
-import {xEvent} from "../core/events.mjs"
+import {xEvent} from "../events/x.mjs"
 import {newSphere} from "./sphere.mjs"
 import {regionsColourSwitch} from "../colors/switches/regions.mjs"
 import {metaUpdate, metaClear} from "../../../libs/mjs/panels/meta.mjs"
 
-export function addPlot(r=1, x=0, y=0, z=0, color=black, code=null) {
+export function newPlot(r=1, x=0, y=0, z=0, color=black, code=null) {
 
   let plot = newSphere(r, x, y, z, regionsColourSwitch(color))
   xEvent(plot, "mouseover", metaUpdate, code)
   xEvent(plot, "mouseout", metaClear)
+  return plot
+
+}
+
+export function addPlot(r=1, x=0, y=0, z=0, color=black, code=null) {
+
+  let plot = newPlot(r, x, y, z, color, code)
   data.plot.core.scene.add(plot)
   return plot
 
