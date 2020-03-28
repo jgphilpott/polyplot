5bf6ed56f861d04d282df17f562d1f5a35cb2be0
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-27 21:01:09 -0400

    mongo maps update

---
+++ app/back/mongo/data/collect/maps/mongo.py
@@ -0,0 +1,3 @@
+from pprint import pprint as pp
+from back.mongo.data.collect.ions import find_collection
+from back.mongo.data.collect.maps.object import Map
---
+++ app/back/mongo/data/collect/maps/object.py
@@ -0,0 +1,7 @@
+class Map():
+
+    def __init__(self, map):
+
+        self.code = map["code"]
+        self.name = map["name"]
+        self.coordinates = map["coordinates"]
