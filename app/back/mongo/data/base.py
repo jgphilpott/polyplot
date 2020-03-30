cd02515bc2fa46dddda22a137d4795f6a2c8b201
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-30 04:11:07 -0400

    mongo update

--- app/back/mongo/data/base.py
+++ app/back/mongo/data/base.py
@@ -1,32 +1,12 @@
 from back.mongo.client import find_client
-from back.tools.alarm.warning import warning_loop
 
 client = find_client()
 database = client.polyplot
 
-def find_database(log=False):
+def find_database():
 
-    if log:
+    return database
 
-        print("\n\033[93mThe mongo database:\033[0m\n\n{}\n".format(database))
+def drop_database():
 
-    else:
-
-        return database
-
-def drop_database(log=False):
-
-    if log:
-
-        warning = "\n\033[91mWarning! Are you sure you want to drop the database?\033[0m\n"
-        message = "\n\033[93mOkay, the database has been droped!\033[0m\n"
-
-        reply = warning_loop(warning, message)
-
-        if reply:
-
-            client.drop_database(database)
-
-    else:
-
-        return client.drop_database(database)
+    return client.drop_database(database)
