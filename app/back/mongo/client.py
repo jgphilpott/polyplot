a9ff9ff3594f305785fff78ed35871ce0190873d
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-29 21:52:57 -0400

    mongo client update

--- app/back/mongo/client.py
+++ app/back/mongo/client.py
@@ -5,10 +5,4 @@ client = MongoClient(environ["MONGO_PORT_27017_TCP_ADDR"], 27017)
 
 def find_client(log=False):
 
-    if log:
-
-        print("\n\033[93mThe mongo client:\033[0m\n\n{}\n".format(client))
-
-    else:
-
-        return client
+    return client
