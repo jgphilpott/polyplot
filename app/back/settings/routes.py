29c6bdcdf024de35692c00e21c03ba55ba4d99fc
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-29 21:47:20 -0400

    routes update

--- app/back/settings/routes.py
+++ app/back/settings/routes.py
@@ -4,11 +4,11 @@ from front.tree.home.poly3.route import register_poly3_route
 from front.tree.home.poly2.route import register_poly2_route
 from front.tree.home.map.route import register_map_route
 
-from front.tree.home.countries.route import register_countries_route
 from front.tree.home.countries.country.route import register_country_route
+from front.tree.home.countries.route import register_countries_route
 
-from front.tree.home.indicators.route import register_indicators_route
 from front.tree.home.indicators.indicator.route import register_indicator_route
+from front.tree.home.indicators.route import register_indicators_route
 
 from front.tree.errors._404_.route import register_404_error_route
 from front.tree.errors._500_.route import register_500_error_route
@@ -21,11 +21,11 @@ def register(app):
     register_poly2_route(app)
     register_map_route(app)
 
-    register_countries_route(app)
     register_country_route(app)
+    register_countries_route(app)
 
-    register_indicators_route(app)
     register_indicator_route(app)
+    register_indicators_route(app)
 
     register_404_error_route(app)
     register_500_error_route(app)
