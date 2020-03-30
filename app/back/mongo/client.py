f1875c614aeaf93793b3a0e5e53fbe23c10fac10
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-30 04:39:45 -0400

    client update

--- app/back/mongo/client.py
+++ app/back/mongo/client.py
@@ -1,8 +1,6 @@
 from os import environ
 from pymongo import MongoClient
 
-client = MongoClient(environ["MONGO_PORT_27017_TCP_ADDR"], 27017)
+def find_client():
 
-def find_client(log=False):
-
-    return client
+    return MongoClient(environ["MONGO_PORT_27017_TCP_ADDR"], 27017)
