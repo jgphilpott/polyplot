5353dbec37f7fc72230add51596675d49f798b95
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-31 02:50:24 -0400

    map update

--- app/front/tree/home/map/route.py
+++ app/front/tree/home/map/route.py
@@ -1,10 +1,12 @@
 from flask import render_template
 
+from back.mongo.data.collect.maps import find_maps
+
 def register_map_route(app):
 
     @app.route("/map")
     def map():
 
-        data = None
+        data = find_maps()
 
         return render_template("tree/home/map/page.html", data=data)
--- app/front/tree/home/map/script.mjs
+++ app/front/tree/home/map/script.mjs
@@ -1,1 +1,11 @@
-$(document).ready(function() {})
+$(document).ready(function() {
+
+  let projection = d3.geoEquirectangular()
+
+  let geoGenerator = d3.geoPath().projection(projection)
+
+  let svg = d3.select("#canvas").selectAll("path").data(data)
+
+  svg.enter().append("path").attr("d", geoGenerator)
+
+})
