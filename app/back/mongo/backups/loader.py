729da74db665e5edcb5ca2d5c2cecb0e7ffc07ae
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-30 22:33:44 -0400

    loader update

--- app/back/mongo/backups/loader.py
+++ app/back/mongo/backups/loader.py
@@ -1,11 +1,9 @@
-from threading import Thread
-
 from back.mongo.backups.js import load_js
 from back.mongo.backups.json import load_json
 from back.mongo.backups.sass import load_sass
 
 def load_all(path):
 
-    Thread(target=load_js, args=(path,)).start()
-    Thread(target=load_json, args=(path,)).start()
-    Thread(target=load_sass, args=(path,)).start()
+    load_js(path)
+    load_json(path)
+    load_sass(path)
