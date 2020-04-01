cc3d6fa30749c4854b92c93d62291036587927a6
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-31 21:37:18 -0400

    materials update

--- app/front/libs/mjs/materials/line.mjs
+++ app/front/libs/mjs/materials/line.mjs
@@ -2,6 +2,6 @@ import {black} from "../colors/three/grayscale.mjs"
 
 export function newLineMaterial(color=black, linewidth=1) {
 
-  return new THREE.LineBasicMaterial({color: color, linewidth: linewidth})
+  return new THREE.LineBasicMaterial({"color": color, "linewidth": linewidth})
 
 }
--- app/front/libs/mjs/materials/mesh.mjs
+++ app/front/libs/mjs/materials/mesh.mjs
@@ -2,6 +2,6 @@ import {black} from "../colors/three/grayscale.mjs"
 
 export function newMeshMaterial(color=black) {
 
-  return new THREE.MeshStandardMaterial({color: color})
+  return new THREE.MeshStandardMaterial({"color": color})
 
 }
