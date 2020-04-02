14868c1e5b2c6133d787dbaaa335e6f23005d083
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-02 15:17:33 -0400

    setting up ploy2 for scale

--- app/front/tree/home/poly2/route.py
+++ app/front/tree/home/poly2/route.py
@@ -1,10 +1,15 @@
 from flask import render_template
 
+from back.mongo.data.collect.countries import find_countries
+from back.mongo.data.collect.indicators import find_indicator
+
 def register_poly2_route(app):
 
     @app.route("/poly2")
     def poly2():
 
-        data = None
+        data = {}
+        data["plot"] = {"title": "World Bank Development Indicators"}
+        data["plot"]["time"] = {"yearMin": 1960, "year": 1990, "yearMax": 2019}
 
         return render_template("tree/home/poly2/page.html", data=data)
--- app/front/tree/home/poly2/script.mjs
+++ app/front/tree/home/poly2/script.mjs
@@ -1,1 +1,5 @@
-$(document).ready(function() {})
+$(document).ready(function() {
+
+  console.log(data)
+
+})
