5c07a8dc96d2f777dc5c50140dc389ae28c51056
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-02 15:18:10 -0400

    poly3 touch ups

--- app/front/tree/home/poly3/route.py
+++ app/front/tree/home/poly3/route.py
@@ -8,35 +8,29 @@ def register_poly3_route(app):
     @app.route("/poly3")
     def poly3():
 
-        try:
+        data = {}
+        data["plot"] = {"title": "World Bank Development Indicators"}
+        data["plot"]["time"] = {"yearMin": 1960, "year": 1990, "yearMax": 2019}
 
-            data = {}
-            data["plot"] = {"title": "World Bank Development Indicators"}
-            data["plot"]["time"] = {"yearMin": 1960, "year": 1990, "yearMax": 2019}
+        r = find_indicator(request.args["r"]) if "r" in request.args else find_indicator("SP.POP.TOTL")
+        x = find_indicator(request.args["x"]) if "x" in request.args else find_indicator("SP.DYN.LE00.IN")
+        y = find_indicator(request.args["y"]) if "y" in request.args else find_indicator("SP.DYN.TFRT.IN")
+        z = find_indicator(request.args["z"]) if "z" in request.args else find_indicator("NY.GDP.PCAP.KD.ZG")
 
-            r = find_indicator(request.args["r"]) if "r" in request.args else find_indicator("SP.POP.TOTL")
-            x = find_indicator(request.args["x"]) if "x" in request.args else find_indicator("SP.DYN.LE00.IN")
-            y = find_indicator(request.args["y"]) if "y" in request.args else find_indicator("SP.DYN.TFRT.IN")
-            z = find_indicator(request.args["z"]) if "z" in request.args else find_indicator("NY.GDP.PCAP.KD.ZG")
+        data["plot"]["r"] = {"name": r["name"]}
+        data["plot"]["x"] = {"name": x["name"]}
+        data["plot"]["y"] = {"name": y["name"]}
+        data["plot"]["z"] = {"name": z["name"]}
 
-            data["plot"]["r"] = {"name": r["name"]}
-            data["plot"]["x"] = {"name": x["name"]}
-            data["plot"]["y"] = {"name": y["name"]}
-            data["plot"]["z"] = {"name": z["name"]}
+        countries = find_countries()
 
-            countries = find_countries()
+        for country in countries:
 
-            for country in countries:
+            country["r"] = [index for index in r["geographies"] if index["code"] in [country["code"]]][0]["history"]
+            country["x"] = [index for index in x["geographies"] if index["code"] in [country["code"]]][0]["history"]
+            country["y"] = [index for index in y["geographies"] if index["code"] in [country["code"]]][0]["history"]
+            country["z"] = [index for index in z["geographies"] if index["code"] in [country["code"]]][0]["history"]
 
-                country["r"] = [index for index in r["geographies"] if index["code"] in [country["code"]]][0]["history"]
-                country["x"] = [index for index in x["geographies"] if index["code"] in [country["code"]]][0]["history"]
-                country["y"] = [index for index in y["geographies"] if index["code"] in [country["code"]]][0]["history"]
-                country["z"] = [index for index in z["geographies"] if index["code"] in [country["code"]]][0]["history"]
-
-            data["plot"]["plots"] = countries
-
-        except:
-
-            data = None
+        data["plot"]["plots"] = countries
 
         return render_template("tree/home/poly3/page.html", data=data)
--- app/front/tree/home/poly3/script.mjs
+++ app/front/tree/home/poly3/script.mjs
@@ -71,4 +71,6 @@ $(document).ready(function() {
 
   animatePlot()
 
+  console.log(data)
+
 })
