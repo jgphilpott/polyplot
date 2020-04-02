2fbc128e050c8268bda3bb27738f056f1d4e4647
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-01 20:28:17 -0400

    map update

--- app/front/tree/home/map/script.mjs
+++ app/front/tree/home/map/script.mjs
@@ -1,8 +1,16 @@
+import {width, height} from "../../../libs/mjs/env/dimensions.mjs"
+
 $(document).ready(function() {
 
   console.log(data)
 
-  let projection = d3.geoEquirectangular()
+  let geoJSON = {"type": "FeatureCollection", "features": data}
+
+  let geoMercator = d3.geoMercator()
+  let geoOrthographic = d3.geoOrthographic()
+  let geoEquirectangular = d3.geoEquirectangular()
+
+  let projection = geoEquirectangular.fitExtent([[0, 0], [width(), height()]], geoJSON)
 
   let geoGenerator = d3.geoPath().projection(projection)
 
