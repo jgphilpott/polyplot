eae2c5077d4cca8325ee32a04625c3caa2246792
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-04 01:40:27 -0400

    errors update

--- app/front/tree/errors/_500_/script.mjs
+++ app/front/tree/errors/_500_/script.mjs
@@ -1,7 +1,13 @@
 import {rainbow} from "../../../libs/mjs/colors/solid/rainbow.mjs"
 
+import {addErrorPanel} from "../../../libs/mjs/panels/error.mjs"
+import {makePanelsDragable} from "../../../libs/mjs/panels/all.mjs"
+
 $(document).ready(function() {
 
   summonParticleWeb(42, rainbow)
+
+  addErrorPanel()
+  makePanelsDragable()
 
 })
