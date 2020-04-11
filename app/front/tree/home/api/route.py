7f2369cd4e0692afe9255d3906c2b7951f67d035
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-11 14:01:24 -0400

    api update

--- app/front/tree/home/api/route.py
+++ app/front/tree/home/api/route.py
@@ -5,6 +5,4 @@ def register_api_route(app):
     @app.route("/api")
     def api():
 
-        data = None
-
         return redirect("https://github.com/jgphilpott/polyplot/blob/master/docs/api/README.md")
