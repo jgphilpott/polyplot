550d0799038dc3adc584ed5043b7150624c5c864
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-10 21:53:53 -0400

    countries class update

--- app/back/mongo/data/collect/countries/object.py
+++ app/back/mongo/data/collect/countries/object.py
@@ -3,5 +3,6 @@ class Country():
     def __init__(self, country):
 
         self.code = country["code"]
+        self.name = country["formal_name"]
         self.name = country["name"]
         self.region = country["region"]
