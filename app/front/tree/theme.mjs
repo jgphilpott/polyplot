8efb1457254ae2a89b458b66ab9cba4e4cceb70e
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-15 18:27:51 -0400

    theme update

--- app/front/tree/theme.mjs
+++ app/front/tree/theme.mjs
@@ -4,4 +4,26 @@ $(document).ready(function() {
 
   const socket = io()
 
+  $("#nav #menu").click(function() {
+
+    let error = $("#error.panel")
+    let home = $("#home.panel")
+    let menu = $("#menu.panel")
+
+    if (menu.css("visibility") == "hidden") {
+
+      error.css("visibility", "hidden")
+      home.css("visibility", "hidden")
+      menu.css("visibility", "visible")
+
+    } else {
+
+      error.css("visibility", "visible")
+      home.css("visibility", "visible")
+      menu.css("visibility", "hidden")
+
+    }
+
+  })
+
 })
