a1a7a4256276e9ccbcd5cd0a1f708c482b3122c8
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-15 03:02:10 -0400

    legend update

--- app/front/libs/mjs/panels/legend.mjs
+++ app/front/libs/mjs/panels/legend.mjs
@@ -1,10 +1,14 @@
 export function addLegendPanel() {
 
+  $("body").append("<div id='legend' class='panel'></div>")
+
   let panel = $("#legend.panel")
 
-  $("#legend #r-data").text(data.plot.r.name)
-  $("#legend #x-data").text(data.plot.x.name)
-  $("#legend #y-data").text(data.plot.y.name)
-  $("#legend #z-data").text(data.plot.z.name)
+  panel.append("<h1 id='name'>Legend</h1>")
+
+  panel.append("<p id='r'><strong id='r-key'>R:</strong> <span id='r-data'>" + data.plot.r.name + "</span></p>")
+  panel.append("<p id='x'><strong id='x-key'>X:</strong> <span id='x-data'>" + data.plot.x.name + "</span></p>")
+  panel.append("<p id='y'><strong id='y-key'>Y:</strong> <span id='y-data'>" + data.plot.y.name + "</span></p>")
+  panel.append("<p id='z'><strong id='z-key'>Z:</strong> <span id='z-data'>" + data.plot.z.name + "</span></p>")
 
 }
