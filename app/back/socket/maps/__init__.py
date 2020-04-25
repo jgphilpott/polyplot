c7c3ed32ec81253d33cef6e6fe84b0c247cbfc24
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-25 16:48:01 -0400

    adding maps socket

---
+++ app/back/socket/maps/__init__.py
@@ -0,0 +1,15 @@
+from flask_socketio import emit
+
+from back.mongo.data.collect.maps.mongo import find_map, find_maps
+
+def connect_maps(app):
+
+    @app.on("get_map")
+    def get_map(code, detail="low"):
+
+        emit("new_map", find_map(code, detail))
+
+    @app.on("get_maps")
+    def get_maps(detail="low"):
+
+        emit("new_maps", find_maps(detail))
