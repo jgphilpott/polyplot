8f308f203c0d92e98ebe1553f019cc68544633b5
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-25 16:21:48 -0400

    plug+

--- app/back/socket/plug.py
+++ app/back/socket/plug.py
@@ -1,4 +1,4 @@
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 
 from back.socket.clients import connect_clients
 from back.socket.countries import connect_countries
