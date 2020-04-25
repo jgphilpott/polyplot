b0e635684c80fcbffca42e19ecaa9ccc42b825b3
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-25 19:01:03 -0400

    adding indicators socket

---
+++ app/back/socket/indicators/__init__.py
@@ -0,0 +1,24 @@
+from flask_socketio import emit
+
+from back.mongo.data.collect.indicators.mongo import find_indicator, find_indicators
+
+def connect_indicators(app):
+
+    @app.on("get_indicator")
+    def get_indicator(code):
+
+        emit("new_indicator", find_indicator(code))
+
+    @app.on("get_indicators")
+    def get_indicators():
+
+        available_indicators = []
+
+        for indicator in find_indicators():
+
+            if "geographies" in indicator.keys():
+
+                del indicator["geographies"]
+                available_indicators.append(indicator)
+
+        emit("new_indicators", available_indicators)
