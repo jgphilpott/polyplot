32a86dd8f1b6cc41248661f97c25755aac07da68
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-25 06:25:36 -0400

    mongo update

--- app/back/mongo/data/collect/__init__.py
+++ app/back/mongo/data/collect/__init__.py
@@ -1,3 +1,4 @@
+from back.mongo.data.collect.clients import *
 from back.mongo.data.collect.countries import *
 from back.mongo.data.collect.indicators import *
 from back.mongo.data.collect.maps import *
