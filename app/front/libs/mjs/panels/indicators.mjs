3a67a049b051842802b0589dde5a687922046e1d
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-26 03:11:50 -0400

    indicators panel update

--- app/front/libs/mjs/panels/indicators.mjs
+++ app/front/libs/mjs/panels/indicators.mjs
@@ -6,13 +6,61 @@ export function addIndicatorsPanel() {
 
   let panel = $("#indicators.panel")
 
-  panel.append("<h1 id='title'>Indicators</h1>")
+  panel.append("<h1 id='title'>Indicators by Category</h1>")
 
   socket.emit("get_indicators")
 
   socket.on("new_indicators", function(indicators) {
 
-    console.log(indicators)
+    function organizeIndicators() {
+
+      let categories = []
+
+      for (let i = 0; i < indicators.length; i++) {
+
+        for (let j = 0; j < indicators[i].categories.length; j++) {
+
+          if (categories.some(category => category.category == indicators[i].categories[j])) {
+
+            let category = categories.find(category => category.category == indicators[i].categories[j])
+
+            category.indicators.push(indicators[i])
+
+            category.indicators.sort((a, b) => (a.name > b.name) ? 1 : -1)
+
+          } else {
+
+            categories.push({"category": indicators[i].categories[j], "indicators": [indicators[i]]})
+
+          }
+
+        }
+
+      }
+
+      return categories.sort((a, b) => (a.category > b.category) ? 1 : -1)
+
+    }
+
+    function drawIndicators() {
+
+      let indicatorCategories = organizeIndicators()
+
+      for (let i = 0; i < indicatorCategories.length; i++) {
+
+        panel.append("<h3>" + indicatorCategories[i].category + "</h3>")
+
+        for (let j = 0; j < indicatorCategories[i].indicators.length; j++) {
+
+          panel.append("<p>" + indicatorCategories[i].indicators[j].name + "</p>")
+
+        }
+
+      }
+
+    }
+
+    drawIndicators()
 
   })
 
