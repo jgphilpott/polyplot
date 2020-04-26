387db5d3bddb2039e5fb63e384cd796c643a4177
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-26 01:10:50 -0400

    map panel update

--- app/front/libs/mjs/panels/map.mjs
+++ app/front/libs/mjs/panels/map.mjs
@@ -2,7 +2,11 @@ import {makeDragable} from "../ui/dragable.mjs"
 
 export function addMapPanel() {
 
+  $("body").append("<div id='map' class='panel'></div>")
+
   let panel = $("#map.panel")
+
+  panel.append("<h1 id='title'>Map</h1>")
 
   makeDragable(panel)
 
