7e275a227525786b1292e551e2df814b3b92021c
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-04-26 01:10:18 -0400

    line panel update

--- app/front/libs/mjs/panels/line.mjs
+++ app/front/libs/mjs/panels/line.mjs
@@ -2,7 +2,11 @@ import {makeDragable} from "../ui/dragable.mjs"
 
 export function addLinePanel() {
 
+  $("body").append("<div id='line' class='panel'></div>")
+
   let panel = $("#line.panel")
+
+  panel.append("<h1 id='title'>Line</h1>")
 
   makeDragable(panel)
 
