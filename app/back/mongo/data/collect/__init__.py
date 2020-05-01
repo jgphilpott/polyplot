bdd422c2995fad3c564a784e63a2f1758000cfa6
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-05-01 08:05:20 -0400

    adding meta

--- app/back/mongo/data/collect/__init__.py
+++ app/back/mongo/data/collect/__init__.py
@@ -2,3 +2,4 @@ from back.mongo.data.collect.clients import *
 from back.mongo.data.collect.countries import *
 from back.mongo.data.collect.indicators import *
 from back.mongo.data.collect.maps import *
+from back.mongo.data.collect.meta import *
---
+++ app/back/mongo/data/collect/meta/__init__.py
@@ -0,0 +1,2 @@
+from back.mongo.data.collect.meta.mongo import *
+from back.mongo.data.collect.meta.object import *
