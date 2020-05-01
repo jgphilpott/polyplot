5f45fde997234f41459d384e65c68734a1610e6d
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-05-01 10:05:07 -0400

    clients update

--- app/back/mongo/data/collect/clients/mongo.py
+++ app/back/mongo/data/collect/clients/mongo.py
@@ -1,22 +1,12 @@
-from sys import getsizeof
-
 from back.mongo.data.collect.ions import find_collection
 from back.mongo.data.collect.clients.object import Client
 
 collection = find_collection("clients")
 
-def find_client(name):
+def find_client(query={}, filter={"_id": 0}):
 
-    return dict(collection.find_one({"name": name}, {"_id": 0}))
+    return dict(collection.find_one(query, filter))
 
-def find_clients():
+def find_clients(query={}, filter={"_id": 0}):
 
-    return list(collection.find({}, {"_id": 0}))
-
-def find_client_size(name):
-
-    return getsizeof(str(find_client(name)))
-
-def find_clients_size():
-
-    return getsizeof(str(find_clients()))
+    return list(collection.find(query, filter))
--- app/back/mongo/data/collect/clients/object.py
+++ app/back/mongo/data/collect/clients/object.py
@@ -2,4 +2,6 @@ class Client():
 
     def __init__(self, client):
 
+        self.name = client["email"]
         self.name = client["name"]
+        self.name = client["password"]
