4c179bcca7814691e2e64304f64cd832d86e96c4
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-05-01 07:53:59 -0400

    loader update

--- app/back/mongo/backups/json/__init__.py
+++ app/back/mongo/backups/json/__init__.py
@@ -1,7 +1,7 @@
 from json import load
 
 from back.mongo.data.collect.ions import find_collection, find_collections
-from back.mongo.data.collect.indicators.mongo import update_indicator
+from back.mongo.data.collect.indicators.mongo import find_indicators, update_indicator
 
 collections = find_collections()
 
@@ -9,38 +9,22 @@ def load_json(path):
 
     path = "/root/app/back/mongo/backups/json"
 
-    if "countries" not in collections:
+    datasets = ["clients", "countries", "indicators", "maps", "meta"]
 
-        collection = find_collection("countries")
-        file = path + "/countries.json"
+    for dataset in datasets:
 
-        with open(file) as list:
+        if dataset not in collections:
 
-            countries = load(list)
-            collection.insert_many(countries)
+            collection = find_collection(dataset)
+            file = path + "/" + dataset + ".json"
 
-    if "indicators" not in collections:
+            with open(file) as list:
 
-        collection = find_collection("indicators")
-        file = path + "/indicators.json"
+                data = load(list)
+                collection.insert_many(data)
 
-        with open(file) as list:
+    for indicator in find_indicators():
 
-            indicators = load(list)
-            collection.insert_many(indicators)
+        if indicator["default"] == True:
 
-        for indicator in indicators:
-
-            if indicator["default"] == True:
-
-                update_indicator(indicator["code"])
-
-    if "maps" not in collections:
-
-        collection = find_collection("maps")
-        file = path + "/maps.json"
-
-        with open(file) as list:
-
-            maps = load(list)
-            collection.insert_many(maps)
+            update_indicator(indicator["code"])
