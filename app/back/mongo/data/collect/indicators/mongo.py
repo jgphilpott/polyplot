816b5f633e21e8a12318e56426ff5c572f70838c
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-05-01 10:28:33 -0400

    indicators update

--- app/back/mongo/data/collect/indicators/mongo.py
+++ app/back/mongo/data/collect/indicators/mongo.py
@@ -1,21 +1,19 @@
-from sys import getsizeof
-
 from back.mongo.data.collect.ions import find_collection
 from back.mongo.data.collect.indicators.object import Indicator
 
 collection = find_collection("indicators")
 
-def find_indicator(code):
+def find_indicator(query={}, filter={"_id": 0}):
 
-    return dict(collection.find_one({"code": code}, {"_id": 0}))
+    return dict(collection.find_one(query, filter))
 
-def find_indicators():
+def find_indicators(query={}, filter={"_id": 0}):
 
-    return list(collection.find({}, {"_id": 0}))
+    return list(collection.find(query, filter))
 
 def update_indicator(code):
 
-    indicator = Indicator(find_indicator(code)).update()
+    indicator = Indicator(find_indicator({"code": code})).update()
 
     collection.update_one({"code": indicator.code}, {"$set": indicator.__dict__})
 
@@ -24,11 +22,3 @@ def update_indicators():
     for indicator in find_indicators():
 
         update_indicator(indicator["code"])
-
-def find_indicator_size(code):
-
-    return getsizeof(str(find_indicator(code)))
-
-def find_indicators_size():
-
-    return getsizeof(str(find_indicators()))
