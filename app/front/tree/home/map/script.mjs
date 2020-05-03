bfa2d81ff68b813b64c2a95e959c8584f7d5e2a8
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-05-03 04:02:22 -0400

    map update

--- app/front/tree/home/map/script.mjs
+++ app/front/tree/home/map/script.mjs
@@ -1,83 +1,23 @@
-import {scaleAxes} from "../../../libs/mjs/scales/axes.mjs"
 import {addAllPanels} from "../../../libs/mjs/panels/all.mjs"
-import {width, height} from "../../../libs/mjs/env/dimensions.mjs"
-import {updateMetaPanel, clearMetaPanel} from "../../../libs/mjs/panels/meta.mjs"
+import {scaleAxes} from "../../../libs/mjs/scales/axes.mjs"
+import {drawMaps} from "../../../libs/mjs/draw/maps.mjs"
+import {makeZoomable} from "../../../libs/mjs/cartography/zoom.mjs"
+
+let plot = data.plot
+let plotType = plot.type
 
 $(document).ready(function() {
-
-  let plot = data.plot
 
   $("body").append("<svg id='canvas'></svg>")
 
   let canvas = d3.select("#canvas")
 
-  addAllPanels()
+  addAllPanels(plotType)
 
-  let geoMercator = d3.geoMercator()
-  let geoOrthographic = d3.geoOrthographic()
-  let geoEquirectangular = d3.geoEquirectangular()
+  scaleAxes(plotType)
 
-  let projection = geoEquirectangular.fitSize([width(), height()], plot.GeoJSON)
+  drawMaps(plotType)
 
-  let path = d3.geoPath().projection(projection)
-
-  scaleAxes()
-
-  canvas.selectAll(".map")
-        .data(plot.GeoJSON.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .attr("id", function(feature) {
-
-          return feature.properties.code
-
-        })
-        .attr("class", "map")
-        .attr("fill", function(feature) {
-
-          let history = plot.plots.find(plot => plot.code == feature.properties.code).x
-          let value = history.find(year => year.year == plot.t.year).value
-
-          if (value) {
-
-            return plot.x.scale(value)
-
-          } else {
-
-            return "gray"
-
-          }
-
-        })
-
-  let plots = $(".map")
-
-  for (let i = 0; i < plots.length; i++) {
-
-    $("#" + plots[i].id + ".map").mouseenter(function() {
-
-      updateMetaPanel(plots[i].id)
-
-    }).mouseleave(function() {
-
-      clearMetaPanel()
-
-    })
-
-  }
-
-  let zoom = d3.zoom()
-               .scaleExtent([1, 42])
-               .translateExtent([[0, 0], [width(), height()]])
-               .on("zoom", function() {
-
-                 d3.selectAll("path").attr("transform", d3.event.transform)
-
-               })
-
-  canvas.call(zoom)
-
-  console.log(data)
+  makeZoomable(canvas)
 
 })
