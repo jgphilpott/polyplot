67f4b09338aaab49df164fc43a4f6aca6468c130
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-05-03 23:23:52 -0400

    colors update

---
+++ app/front/libs/mjs/colors/solid/rainbow.mjs
@@ -0,0 +1,9 @@
+export let red = "rgba(224, 58, 62)"
+export let orange = "rgba(245, 130, 31)"
+export let yellow = "rgba(253, 184, 39)"
+export let green = "rgba(97, 187, 70)"
+export let blue = "rgba(0, 157, 220)"
+export let purple = "rgba(150, 61, 151)"
+export let pink = "rgba(225, 67, 117)"
+
+export let rainbow = [red, orange, yellow, green, blue, purple, pink]
