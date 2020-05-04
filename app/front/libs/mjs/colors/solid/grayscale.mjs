b9e00d4708817f7fa915054cf2cd37bf2a58d544
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-05-04 07:54:22 -0400

    colors typo fix

--- app/front/libs/mjs/colors/solid/grayscale.mjs
+++ app/front/libs/mjs/colors/solid/grayscale.mjs
@@ -1,7 +1,7 @@
 export let black = "rgba(0, 0, 0)"
-export let darkGrey = "rgba(179, 179, 179)"
-export let grey = "rgba(204, 204, 204)"
-export let lightGrey = "rgba(230, 230, 230)"
+export let darkGray = "rgba(179, 179, 179)"
+export let gray = "rgba(204, 204, 204)"
+export let lightGray = "rgba(230, 230, 230)"
 export let white = "rgba(255, 255, 255)"
 
-export let grayscale = [black, darkGrey, grey, lightGrey, white]
+export let grayscale = [black, darkGray, gray, lightGray, white]
