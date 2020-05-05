6d0525ec420927e9901a7e43df63a3c0efd9589a
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-05-05 01:21:20 -0400

    working on three setup

---
+++ app/front/libs/mjs/three/setup.mjs
@@ -0,0 +1,28 @@
+import {newRenderer} from "../../../libs/mjs/three/renderer.mjs"
+import {newCanvas} from "../../../libs/mjs/three/canvas.mjs"
+import {newScene} from "../../../libs/mjs/three/scene.mjs"
+
+import {addLights} from "../../../libs/mjs/lights/all.mjs"
+import {addCamera} from "../../../libs/mjs/cameras/all.mjs"
+import {addControls} from "../../../libs/mjs/controls/all.mjs"
+
+let core = {}
+data.plot.core = core
+
+export function setup() {
+
+  // Setup
+  core.renderer = newRenderer()
+  core.canvas = newCanvas()
+  core.scene = newScene()
+
+  // Lights
+  core.lights = addLights()
+
+  // Camera
+  core.camera = addCamera()
+
+  // Action
+  core.controls = addControls()
+
+}
