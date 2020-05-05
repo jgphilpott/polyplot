f64585b7b2cc94a9b12c2655c09d1cb9cc4b57fb
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-05-05 02:36:05 -0400

    controls update

---
+++ app/front/libs/mjs/controls/all.mjs
@@ -0,0 +1,9 @@
+import {addOrbitControls} from "./orbit.mjs"
+
+export function addControls() {
+
+    let controls = addOrbitControls()
+
+    return controls
+
+}
---
+++ app/front/libs/mjs/controls/orbit.mjs
@@ -0,0 +1,5 @@
+export function addOrbitControls() {
+
+  return new THREE.OrbitControls(data.plot.core.camera, data.plot.core.canvas)
+
+}
