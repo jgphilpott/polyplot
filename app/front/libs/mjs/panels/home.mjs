a185df549be1b6cc78e551c95b20a3c5f7c1a00b
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-05-08 22:56:09 -0400

    home panel update

---
+++ app/front/libs/mjs/panels/home.mjs
@@ -0,0 +1,20 @@
+import {makeDragable} from "./events/drag.mjs"
+import {addPanelHover} from "./events/hover.mjs"
+
+export function addHomePanel() {
+
+  $("body").append("<div id='home' class='panel'></div>")
+
+  let panel = $("#home.panel")
+
+  panel.append("<img class='close' src='/front/imgs/panels/all/close.png'>")
+
+  panel.append("<img id='logo' src='/front/imgs/theme/logo.png'>")
+  panel.append("<h1 id='title'>Polyplot</h1>")
+  panel.append("<p id='description'>A data exploration application inspired by <a href='https://github.com/olarosling'>Ola Rosling</a>'s <a href='https://en.wikipedia.org/wiki/Trendalyzer'>Trendalyzer</a> software.</p>")
+  panel.append("<iframe id='video' src='https://www.youtube.com/embed/placeholder' allowfullscreen></iframe>")
+
+  makeDragable(panel)
+  addPanelHover(panel)
+
+}
