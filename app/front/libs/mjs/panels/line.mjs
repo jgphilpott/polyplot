8258e6f451fd536f96a30d7158c45cd26d03708b
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-11-08 09:06:00 -0500

    organizing

--- app/front/libs/mjs/panels/line.mjs
+++ app/front/libs/mjs/panels/line.mjs
@@ -8,7 +8,7 @@ export function addLinePanel() {
 
   panel.append("<img class='close' src='/front/imgs/panels/all/close.png'>")
 
-  panel.append("<h1 id='title'>Line</h1>")
+  panel.append("<h1 id='name'>Line</h1>")
 
   addPanelEvents(panel)
 
