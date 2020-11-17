9ad9bd40f90f2cecec1bd9af4fe5713e91fcae96
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-11-17 04:58:26 -0500

    socket client update

--- app/back/socket/clients/__init__.py
+++ app/back/socket/clients/__init__.py
@@ -1,6 +1,7 @@
+from hashlib import sha256
 from flask_socketio import emit
 
-from back.mongo.data.collect.clients.mongo import find_client, find_clients
+from back.mongo.data.collect.clients.mongo import Client, new_client, find_client, update_client
 
 clients = 0
 
@@ -17,3 +18,41 @@ def connect_clients(app):
 
         global clients
         clients -= 1
+
+    @app.on("signup")
+    def signup(client):
+
+        try:
+
+            find_client({"email": client["email"]})
+            emit("signup_failed")
+
+        except:
+
+            client["password"] = sha256(client["password"].encode("utf-8")).hexdigest()
+            client = Client(client).refresh_id()
+
+            new_client(client.__dict__)
+            emit("signup_success", client.id)
+
+    @app.on("login")
+    def login(client):
+
+        try:
+
+            match = find_client({"email": client["email"]}, {"_id": 0})
+
+            if match["password"] == sha256(client["password"].encode("utf-8")).hexdigest():
+
+                client = Client(match).refresh_id()
+
+                update_client(client.__dict__)
+                emit("login_success", client.id)
+
+            else:
+
+                emit("login_failed")
+
+        except:
+
+            emit("login_failed")
