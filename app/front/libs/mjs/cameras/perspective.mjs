b9c11b4fb6b91589ece1de23b2a741e2097c1454
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-11-26 08:44:56 -0500

    camera update

--- app/front/libs/mjs/cameras/perspective.mjs
+++ app/front/libs/mjs/cameras/perspective.mjs
@@ -1,9 +1,15 @@
 import {min, max, width, height} from "../env/window.mjs"
 
-export function addPerspectiveCamera(fov=75, near=(max / 1000), far=(max * 10), x=max, y=max, z=max) {
+export function addPerspectiveCamera(fov=75, near=(max / 1000), far=(max * 10)) {
 
   let camera = new THREE.PerspectiveCamera(fov, width() / height(), near, far)
-  camera.position.set(x, y, z)
+
+  camera.position.set(max, max, max)
+  camera.up.set(0, 0, 1)
+
+  camera.target = {"x": min, "y": min, "z": min}
+  camera.lookAt(min, min, min)
+
   return camera
 
 }
