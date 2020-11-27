415e7c6c6765eb03802aad6a969613a8d0ecbcec
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-11-27 09:48:30 -0500

    tree/libs update

--- app/back/mongo/backups/js/__init__.py
+++ app/back/mongo/backups/js/__init__.py
@@ -17,7 +17,6 @@ def load_js(path):
     urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js", path + "/socket.js")
 
     urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js", path + "/jQuery.js")
-    urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js", path + "/jQueryUI.js")
 
     urlretrieve("https://cdnjs.cloudflare.com/ajax/libs/d3/5.15.0/d3.min.js", path + "/d3.js")
 
