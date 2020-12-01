d34a432630a847aacdc68ed0fe52693afd01f0dc
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-12-01 05:38:45 -0500

    adding airports to socket

---
+++ app/back/socket/airports/__init__.py
@@ -0,0 +1,15 @@
+from flask_socketio import emit
+
+from back.mongo.data.collect.airports.mongo import find_airport, find_airports
+
+def connect_airports(app):
+
+    @app.on("get_airport")
+    def get_airport(code):
+
+        emit("new_airport", find_airport({"code": code}))
+
+    @app.on("get_airports")
+    def get_airports():
+
+        emit("new_airports", find_airports())
--- app/back/socket/plug.py
+++ app/back/socket/plug.py
@@ -1,5 +1,6 @@
 from flask_socketio import SocketIO
 
+from back.socket.airports import connect_airports
 from back.socket.clients import connect_clients
 from back.socket.countries import connect_countries
 from back.socket.indicators import connect_indicators
@@ -10,6 +11,7 @@ def plugin(app):
 
     app = SocketIO(app)
 
+    connect_airports(app)
     connect_clients(app)
     connect_countries(app)
     connect_indicators(app)
