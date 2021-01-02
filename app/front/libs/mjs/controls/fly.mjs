6bb604abfcbe570f71b9b259dfc8699d1c047cfc
Jacob Philpott <jacob.gary.philpott@gmail.com>
2021-01-02 04:37:58 -0500

    starting fly controls

---
+++ app/front/libs/mjs/controls/fly.mjs
@@ -0,0 +1,23 @@
+export function addFlyControls() {
+
+  // $(document).keypress(function(event) {
+  //
+  //   if (event.keyCode == 119) { // W
+  //
+  //     camera.target.z += 1
+  //     camera.lookAt(camera.target.x, camera.target.y, camera.target.z)
+  //
+  //   } else if (event.keyCode == 115) { // S
+  //
+  //     camera.target.z -= 1
+  //     camera.lookAt(camera.target.x, camera.target.y, camera.target.z)
+  //
+  //   } else if (event.keyCode == 97) { // A
+  //
+  //   } else if (event.keyCode == 100) { // D
+  //
+  //   }
+  //
+  // })
+
+}
