32d69f23709de863b28a9381722e22c53506578d
Jacob Philpott <jacob.gary.philpott@gmail.com>
2021-01-09 13:41:07 +0300

    indicator/s page updates

--- app/front/tree/home/indicators/indicator/script.mjs
+++ app/front/tree/home/indicators/indicator/script.mjs
@@ -1,1 +1,13 @@
-$(document).ready(function() {})
+import {rainbow} from "../../../../libs/mjs/colors/solid/rainbow.mjs"
+
+$(document).ready(function() {
+
+  $("body").append("<canvas id='canvas'></canvas>")
+
+  summonParticleWeb(42, rainbow)
+
+  $("body").append("<div id='indicator' class='panel'></div>")
+
+  let panel = $("#indicator.panel")
+
+})
--- app/front/tree/home/indicators/script.mjs
+++ app/front/tree/home/indicators/script.mjs
@@ -1,1 +1,13 @@
-$(document).ready(function() {})
+import {rainbow} from "../../../libs/mjs/colors/solid/rainbow.mjs"
+
+$(document).ready(function() {
+
+  $("body").append("<canvas id='canvas'></canvas>")
+
+  summonParticleWeb(42, rainbow)
+
+  $("body").append("<div id='indicators' class='panel'><h1 id='name'>Indicators by Category</h1></div>")
+
+  let panel = $("#indicators.panel")
+
+})
