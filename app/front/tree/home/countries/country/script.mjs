6d39316c84bd17f5fd742c6d7ecc5b3b0d24f5ed
Jacob Philpott <jacob.gary.philpott@gmail.com>
2021-01-17 18:59:31 +0300

    starting country page

--- app/front/tree/home/countries/country/script.mjs
+++ app/front/tree/home/countries/country/script.mjs
@@ -1,4 +1,6 @@
 import {rainbow} from "../../../../libs/mjs/colors/solid/rainbow.mjs"
+import {regionsColourSwitch} from "../../../../libs/mjs/colors/switches/regions.mjs"
+import {makeScrollable} from "../../../../libs/mjs/panels/events/scroll.mjs"
 
 $(document).ready(function() {
 
@@ -6,8 +8,16 @@ $(document).ready(function() {
 
   summonParticleWeb(42, rainbow)
 
-  $("body").append("<div id='country' class='panel'></div>")
+  let country = data.country
+
+  $("body").append("<div id='country' class='panel'><a href='/api/countries/" + country.code + "'><h1 id='name'>" + country.name + "</h1></a></div>")
 
   let panel = $("#country.panel")
+
+  panel.append("<img class='flag' src='/front/imgs/flags/" + country.code + ".png'>")
+  panel.append("<p><b>Formal Name:</b> " + country.formal_name + "</p>")
+  panel.append("<p><b>Region:</b> " + country.region + "</p>")
+
+  makeScrollable(panel)
 
 })
