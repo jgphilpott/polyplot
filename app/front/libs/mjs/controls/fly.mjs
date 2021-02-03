721f09da547b90b93c988a96240bf5200fefbd7b
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-03 12:22:11 +0300

    fly controls update

--- app/front/libs/mjs/controls/fly.mjs
+++ app/front/libs/mjs/controls/fly.mjs
@@ -2,6 +2,9 @@ export function addFlyControls() {
 
   $(document).keydown(function(event) {
 
+    let flySpeed = 1
+    let spinSpeed = 1
+
     let camera = data.plot.core.camera
     let position = camera.position
     let target = camera.target
@@ -11,80 +14,25 @@ export function addFlyControls() {
     let deltaZ = Math.abs(position.z - target.z)
 
     let radius3 = position.distanceTo(target)
-    let radius2 = side4sides(deltaZ, null, radius3)
+    let radius2 = side4sides(deltaY, null, radius3)
 
-    let horizontalAngle = angle4sides(radius2, deltaX)
-    if (position.x < target.x) { horizontalAngle = 180 - horizontalAngle }
-    if (position.y < target.y) { horizontalAngle = -horizontalAngle }
+    let horizontalAngle = angle4sides(radius2, deltaZ)
+    if (position.z < target.z) { horizontalAngle = 180 - horizontalAngle }
+    if (position.x < target.x) { horizontalAngle = -horizontalAngle }
 
-    let verticalAngle = angle4sides(radius3, deltaZ)
-    if (position.z < target.z) { verticalAngle = 180 - verticalAngle }
-
-    if (event.keyCode == 87) { // W
-
-      let newVerticalAngle = verticalAngle + 1
-      if (newVerticalAngle <= 0) { newVerticalAngle = 0.000001 } else if (newVerticalAngle >= 180) { newVerticalAngle = -180 - 0.000001 }
-
-      target.z = position.z - side4angle(newVerticalAngle, radius3, true, null)
-
-      radius2 = side4sides(Math.abs(position.z - target.z), null, radius3)
-
-      target.x = position.x - side4angle(horizontalAngle, radius2, true, null)
-      target.y = position.y - side4angle(horizontalAngle, radius2, null, true)
-
-    } else if (event.keyCode == 83) { // S
-
-      let newVerticalAngle = verticalAngle - 1
-      if (newVerticalAngle <= 0) { newVerticalAngle = 0.000001 } else if (newVerticalAngle >= 180) { newVerticalAngle = -180 - 0.000001 }
-
-      target.z = position.z - side4angle(newVerticalAngle, radius3, true, null)
-
-      radius2 = side4sides(Math.abs(position.z - target.z), null, radius3)
-
-      target.x = position.x - side4angle(horizontalAngle, radius2, true, null)
-      target.y = position.y - side4angle(horizontalAngle, radius2, null, true)
-
-    } else if (event.keyCode == 65) { // A
-
-      let newHorizontalAngle = horizontalAngle + 1
-      if (newHorizontalAngle >= 180) { newHorizontalAngle = newHorizontalAngle - 360 }
-
-      target.x = position.x - side4angle(newHorizontalAngle, radius2, true, null)
-      target.y = position.y - side4angle(newHorizontalAngle, radius2, null, true)
-
-    } else if (event.keyCode == 68) { // D
-
-      let newHorizontalAngle = horizontalAngle - 1
-      if (newHorizontalAngle >= 180) { newHorizontalAngle = newHorizontalAngle - 360 }
-
-      target.x = position.x - side4angle(newHorizontalAngle, radius2, true, null)
-      target.y = position.y - side4angle(newHorizontalAngle, radius2, null, true)
-
-    }
-
-    deltaX = position.x - target.x
-    deltaY = position.y - target.y
-    deltaZ = position.z - target.z
+    let verticalAngle = angle4sides(radius3, deltaY)
+    if (position.y < target.y) { verticalAngle = 180 - verticalAngle }
 
     if (event.keyCode == 38) { // Up
 
-      let stepX = deltaX / radius3
-      let stepY = deltaY / radius3
-      let stepZ = deltaZ / radius3
+      let inverter = -1
+      if (verticalAngle > 90) { verticalAngle = 180 - verticalAngle; inverter = -inverter }
 
-      position.x -= stepX
-      position.y -= stepY
-      position.z -= stepZ
+      let radius2 = side4angle(verticalAngle, flySpeed, true, null)
 
-      target.x -= stepX
-      target.y -= stepY
-      target.z -= stepZ
-
-    } else if (event.keyCode == 40) { // Down
-
-      let stepX = deltaX / radius3
-      let stepY = deltaY / radius3
-      let stepZ = deltaZ / radius3
+      let stepX = side4angle(horizontalAngle, radius2, null, true) * inverter
+      let stepY = side4angle(verticalAngle, flySpeed, null, true)
+      let stepZ = side4angle(horizontalAngle, radius2, true, null) * inverter
 
       position.x += stepX
       position.y += stepY
@@ -94,33 +42,94 @@ export function addFlyControls() {
       target.y += stepY
       target.z += stepZ
 
+    } else if (event.keyCode == 40) { // Down
+
+      let inverter = -1
+      if (verticalAngle > 90) { verticalAngle = 180 - verticalAngle; inverter = -inverter }
+
+      let radius2 = side4angle(verticalAngle, flySpeed, true, null)
+
+      let stepX = side4angle(horizontalAngle, radius2, null, true) * inverter
+      let stepY = side4angle(verticalAngle, flySpeed, null, true)
+      let stepZ = side4angle(horizontalAngle, radius2, true, null) * inverter
+
+      position.x -= stepX
+      position.y -= stepY
+      position.z -= stepZ
+
+      target.x -= stepX
+      target.y -= stepY
+      target.z -= stepZ
+
     } else if (event.keyCode == 37) { // Left
 
       let newHorizontalAngle = horizontalAngle + 90
       if (newHorizontalAngle >= 180) { newHorizontalAngle = newHorizontalAngle - 360 }
 
-      let stepX = side4angle(newHorizontalAngle, 1, true, null)
-      let stepY = side4angle(newHorizontalAngle, 1, null, true)
+      let stepX = side4angle(newHorizontalAngle, flySpeed, null, true)
+      let stepZ = side4angle(newHorizontalAngle, flySpeed, true, null)
 
       position.x -= stepX
-      position.y -= stepY
+      position.z -= stepZ
 
       target.x -= stepX
-      target.y -= stepY
+      target.z -= stepZ
 
     } else if (event.keyCode == 39) { // Right
 
       let newHorizontalAngle = horizontalAngle + 90
       if (newHorizontalAngle >= 180) { newHorizontalAngle = newHorizontalAngle - 360 }
 
-      let stepX = side4angle(newHorizontalAngle, 1, true, null)
-      let stepY = side4angle(newHorizontalAngle, 1, null, true)
+      let stepX = side4angle(newHorizontalAngle, flySpeed, null, true)
+      let stepZ = side4angle(newHorizontalAngle, flySpeed, true, null)
 
       position.x += stepX
-      position.y += stepY
+      position.z += stepZ
 
       target.x += stepX
-      target.y += stepY
+      target.z += stepZ
+
+    }
+
+    if (event.keyCode == 87) { // W
+
+      let newVerticalAngle = verticalAngle + spinSpeed
+      if (newVerticalAngle <= 0) { newVerticalAngle = 0.000001 } else if (newVerticalAngle >= 180) { newVerticalAngle = -180 - 0.000001 }
+
+      target.y = position.y - side4angle(newVerticalAngle, radius3, true, null)
+
+      radius2 = side4sides(Math.abs(position.y - target.y), null, radius3)
+
+      target.x = position.x - side4angle(horizontalAngle, radius2, null, true)
+      target.z = position.z - side4angle(horizontalAngle, radius2, true, null)
+
+    } else if (event.keyCode == 83) { // S
+
+      let newVerticalAngle = verticalAngle - spinSpeed
+      if (newVerticalAngle <= 0) { newVerticalAngle = 0.000001 } else if (newVerticalAngle >= 180) { newVerticalAngle = -180 - 0.000001 }
+
+      target.y = position.y - side4angle(newVerticalAngle, radius3, true, null)
+
+      radius2 = side4sides(Math.abs(position.y - target.y), null, radius3)
+
+      target.x = position.x - side4angle(horizontalAngle, radius2, null, true)
+      target.z = position.z - side4angle(horizontalAngle, radius2, true, null)
+
+    } else if (event.keyCode == 65) { // A
+
+      let newHorizontalAngle = horizontalAngle + spinSpeed
+      if (newHorizontalAngle >= 180) { newHorizontalAngle = newHorizontalAngle - 360 }
+
+      target.x = position.x - side4angle(newHorizontalAngle, radius2, null, true)
+      target.z = position.z - side4angle(newHorizontalAngle, radius2, true, null)
+
+    } else if (event.keyCode == 68) { // D
+
+      let newHorizontalAngle = horizontalAngle - spinSpeed
+      if (newHorizontalAngle >= 180) { newHorizontalAngle = newHorizontalAngle - 360 }
+
+      target.x = position.x - side4angle(newHorizontalAngle, radius2, null, true)
+      target.z = position.z - side4angle(newHorizontalAngle, radius2, true, null)
 
     }
 
