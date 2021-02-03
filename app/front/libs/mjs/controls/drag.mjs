4ba81c36c05c821c762db47f004c0ef38e19da37
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-03 11:30:29 +0300

    drag controls update

--- app/front/libs/mjs/controls/drag.mjs
+++ app/front/libs/mjs/controls/drag.mjs
@@ -1,13 +1,13 @@
 export function addDragControls() {
 
-  let camera = data.plot.core.camera
-  let position = camera.position
-  let target = camera.target
-
   let radius3 = null
   let dragSpeed = 10
   let startX, startY = null
   let horizontalAngle, verticalAngle = null
+
+  let camera = data.plot.core.camera
+  let position = camera.position
+  let target = camera.target
 
   function start(event) {
 
@@ -22,14 +22,14 @@ export function addDragControls() {
     let deltaZ = Math.abs(position.z - target.z)
 
     radius3 = position.distanceTo(target)
-    let radius2 = side4sides(deltaZ, null, radius3)
+    let radius2 = side4sides(deltaY, null, radius3)
 
-    horizontalAngle = angle4sides(radius2, deltaX)
-    if (position.x < target.x) { horizontalAngle = 180 - horizontalAngle }
-    if (position.y < target.y) { horizontalAngle = -horizontalAngle }
+    horizontalAngle = angle4sides(radius2, deltaZ)
+    if (position.z < target.z) { horizontalAngle = 180 - horizontalAngle }
+    if (position.x < target.x) { horizontalAngle = -horizontalAngle }
 
-    verticalAngle = angle4sides(radius3, deltaZ)
-    if (position.z < target.z) { verticalAngle = 180 - verticalAngle }
+    verticalAngle = angle4sides(radius3, deltaY)
+    if (position.y < target.y) { verticalAngle = 180 - verticalAngle }
 
     document.onmousemove = drag
     document.onmouseup = stop
@@ -41,7 +41,7 @@ export function addDragControls() {
     event.preventDefault()
     event.stopPropagation()
 
-    let radius2 = side4sides(Math.abs(position.z - target.z), null, radius3)
+    let radius2 = side4sides(Math.abs(position.y - target.y), null, radius3)
 
     let newHorizontalAngle = horizontalAngle + ((startX - event.pageX) / dragSpeed)
     if (newHorizontalAngle >= 180) { newHorizontalAngle = newHorizontalAngle - 360 }
@@ -49,9 +49,9 @@ export function addDragControls() {
     let newVerticalAngle = verticalAngle + ((startY - event.pageY) / dragSpeed)
     if (newVerticalAngle <= 0) { newVerticalAngle = 0.000001 } else if (newVerticalAngle >= 180) { newVerticalAngle = -180 - 0.000001 }
 
-    let newX = side4angle(newHorizontalAngle, radius2, true, null) + target.x
-    let newY = side4angle(newHorizontalAngle, radius2, null, true) + target.y
-    let newZ = side4angle(newVerticalAngle, radius3, true, null) + target.z
+    let newX = side4angle(newHorizontalAngle, radius2, null, true) + target.x
+    let newY = side4angle(newVerticalAngle, radius3, true, null) + target.y
+    let newZ = side4angle(newHorizontalAngle, radius2, true, null) + target.z
 
     position.set(newX, newY, newZ)
     camera.lookAt(target.x, target.y, target.z)
