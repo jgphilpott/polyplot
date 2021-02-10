44775e330b76a1f760132b5b4f9ecdf015a95906
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-10 23:06:44 +0300

    maps mongo update

--- app/back/mongo/data/collect/maps/mongo.py
+++ app/back/mongo/data/collect/maps/mongo.py
@@ -1,17 +1,19 @@
+from operator import itemgetter
+
 from back.mongo.data.collect.ions import find_collection
 from back.mongo.data.collect.maps.object import Map
 
 collection = find_collection("maps")
 
-def find_map(query={}, filter={"_id": 0}, detail="low"):
+def find_map(query={}, filter={"_id": 0}, detail="micro"):
 
     return dict(collection.find_one(query, filter))["detail"][detail]
 
-def find_maps(query={}, filter={"_id": 0}, sort=[("code", 1)], detail="low"):
+def find_maps(query={}, filter={"_id": 0}, sort=[("code", False)], detail="micro"):
 
     maps = []
 
-    for map in list(collection.find(query, filter).sort(sort)):
+    for map in sorted(list(collection.find(query, filter)), key=itemgetter(sort[0][0]), reverse=sort[0][1]):
 
         maps.append(map["detail"][detail])
 
