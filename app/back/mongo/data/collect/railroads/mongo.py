da137cf1cf945cc3b9a8437ffabb367c7aaa2166
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-11 17:43:45 +0300

    railroads mongo update

--- app/back/mongo/data/collect/railroads/mongo.py
+++ app/back/mongo/data/collect/railroads/mongo.py
@@ -5,10 +5,12 @@ from back.mongo.data.collect.railroads.object import Railroad
 
 collection = find_collection("railroads")
 
-def find_railroad(query={}, filter={"_id": 0}):
+def find_railroad(query={}, filter={"_id": 0}, detail="micro"):
 
-    return dict(collection.find_one(query, filter))
+    return dict(collection.find_one(query, filter))["detail"][detail]
 
-def find_railroads(query={}, filter={"_id": 0}, sort=[("id", False)]):
+def find_railroads(query={}, filter={"_id": 0}, sort=[("id", 1)], detail="micro"):
 
-    return sorted(list(collection.find(query, filter)), key=itemgetter(sort[0][0]), reverse=sort[0][1])
+    collection.create_index(sort)
+
+    return [railroad["detail"][detail] for railroad in list(collection.find(query, filter).sort(sort))]
--- app/back/mongo/data/collect/railroads/object.py
+++ app/back/mongo/data/collect/railroads/object.py
@@ -2,9 +2,6 @@ class Railroad():
 
     def __init__(self, railroad):
 
-        self.electric = railroad["electric"]
         self.geometry = railroad["geometry"]
-        self.id = railroad["id"]
-        self.mult_track = railroad["mult_track"]
-        self.rank = railroad["rank"]
+        self.properties = railroad["properties"]
         self.type = railroad["type"]
