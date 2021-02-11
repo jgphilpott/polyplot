e5fe5564ca71a87f887deee70f16c9d143a3a5b8
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-11 15:10:43 +0300

    lakes mongo update

--- app/back/mongo/data/collect/lakes/mongo.py
+++ app/back/mongo/data/collect/lakes/mongo.py
@@ -3,10 +3,12 @@ from back.mongo.data.collect.lakes.object import Lake
 
 collection = find_collection("lakes")
 
-def find_lake(query={}, filter={"_id": 0}):
+def find_lake(query={}, filter={"_id": 0}, detail="micro"):
 
-    return dict(collection.find_one(query, filter))
+    return dict(collection.find_one(query, filter))["detail"][detail]
 
-def find_lakes(query={}, filter={"_id": 0}, sort=[("id", 1)]):
+def find_lakes(query={}, filter={"_id": 0}, sort=[("id", 1)], detail="micro"):
 
-    return list(collection.find(query, filter).sort(sort))
+    collection.create_index(sort)
+
+    return [lake["detail"][detail] for lake in list(collection.find(query, filter).sort(sort))]
--- app/back/mongo/data/collect/lakes/object.py
+++ app/back/mongo/data/collect/lakes/object.py
@@ -2,11 +2,6 @@ class Lake():
 
     def __init__(self, lake):
 
-        self.category = lake["category"]
         self.geometry = lake["geometry"]
-        self.id = lake["id"]
-        self.name = lake["name"]
-        self.rank = lake["rank"]
+        self.properties = lake["properties"]
         self.type = lake["type"]
-        self.wiki = lake["wiki"]
-        self.year = lake["year"]
