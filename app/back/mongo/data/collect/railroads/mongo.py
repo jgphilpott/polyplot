c5f6bdafeb1ef794e38a1f6800331e8fc43ce709
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-13 00:44:10 +0300

    railroads mongo update

--- app/back/mongo/data/collect/railroads/mongo.py
+++ app/back/mongo/data/collect/railroads/mongo.py
@@ -1,16 +1,16 @@
-from operator import itemgetter
-
 from back.mongo.data.collect.ions import find_collection
 from back.mongo.data.collect.railroads.object import Railroad
 
-collection = find_collection("railroads")
-
 def find_railroad(query={}, filter={"_id": 0}, detail="micro"):
 
-    return dict(collection.find_one(query, filter))["detail"][detail]
+    collection = find_collection("railroads_" + detail)
 
-def find_railroads(query={}, filter={"_id": 0}, sort=[("id", 1)], detail="micro"):
+    return dict(collection.find_one(query, filter))
+
+def find_railroads(query={}, filter={"_id": 0}, sort=[("properties.id", 1)], detail="micro"):
+
+    collection = find_collection("railroads_" + detail)
 
     collection.create_index(sort)
 
-    return [railroad["detail"][detail] for railroad in list(collection.find(query, filter).sort(sort))]
+    return list(collection.find(query, filter).sort(sort))
