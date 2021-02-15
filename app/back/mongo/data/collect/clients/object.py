3372f7227a6b66fba9f7445b86f09529ce4b77bc
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-15 14:55:07 +0300

    clients update

--- app/back/mongo/data/collect/clients/object.py
+++ app/back/mongo/data/collect/clients/object.py
@@ -4,12 +4,10 @@ class Client():
 
     def __init__(self, client):
 
-        if "id" in client:
-
-            self.id = client["id"]
-
         self.email = client["email"]
         self.password = client["password"]
+
+        if "id" in client: self.id = client["id"]
 
         if "settings" in client:
 
