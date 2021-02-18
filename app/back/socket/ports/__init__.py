3ad34d9fc6bc439aa0300734de5c8f194a4e7760
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-18 04:06:39 +0300

    ports socket update

--- app/back/socket/ports/__init__.py
+++ app/back/socket/ports/__init__.py
@@ -5,11 +5,11 @@ from back.mongo.data.collect.ports.mongo import find_port, find_ports
 def connect_ports(app):
 
     @app.on("get_port")
-    def get_port(code):
+    def get_port(query={}, filter={"_id": 0}):
 
-        emit("new_port", find_port({"code": code}))
+        emit("new_port", find_port(query, filter))
 
     @app.on("get_ports")
-    def get_ports(limit):
+    def get_ports(query={}, filter={"_id": 0}, sort=[("properties.flow", -1), ("properties.code", 1)]):
 
-        emit("new_ports", find_ports(limit))
+        emit("new_ports", find_ports(query, filter, [tuple(item) for item in sort]))
