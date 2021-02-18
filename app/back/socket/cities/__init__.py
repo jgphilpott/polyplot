1ca1735c2add2b1f752d355de46e4ed976bde4e4
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-18 03:06:45 +0300

    cities socket update

--- app/back/socket/cities/__init__.py
+++ app/back/socket/cities/__init__.py
@@ -5,11 +5,11 @@ from back.mongo.data.collect.cities.mongo import find_city, find_cities
 def connect_cities(app):
 
     @app.on("get_city")
-    def get_city(id):
+    def get_city(query={}, filter={"_id": 0}):
 
-        emit("new_city", find_city({"id": id}))
+        emit("new_city", find_city(query, filter))
 
     @app.on("get_cities")
-    def get_cities(limit):
+    def get_cities(query={}, filter={"_id": 0}, sort=[("properties.rank", 1), ("properties.pop_avg", -1), ("properties.name", 1)]):
 
-        emit("new_cities", find_cities(limit))
+        emit("new_cities", find_cities(query, filter, [tuple(item) for item in sort]))
