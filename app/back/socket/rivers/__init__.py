f8d7ebd809a74f43bcb79676f17804509a4e04b0
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-18 04:01:48 +0300

    rivers socket update

--- app/back/socket/rivers/__init__.py
+++ app/back/socket/rivers/__init__.py
@@ -5,11 +5,11 @@ from back.mongo.data.collect.rivers.mongo import find_river, find_rivers
 def connect_rivers(app):
 
     @app.on("get_river")
-    def get_river(id):
+    def get_river(query={}, filter={"_id": 0}, detail="micro"):
 
-        emit("new_river", find_river({"id": id}))
+        emit("new_river", find_river(query, filter, detail))
 
     @app.on("get_rivers")
-    def get_rivers(limit):
+    def get_rivers(query={}, filter={"_id": 0}, sort=[("properties.id", 1)], detail="micro"):
 
-        emit("new_rivers", find_rivers(limit))
+        emit("new_rivers", find_rivers(query, filter, [tuple(item) for item in sort], detail))
