f72e87a914b28c3288ec1066e5feda96dde6fb3c
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-18 04:32:23 +0300

    lakes socket update

--- app/back/socket/lakes/__init__.py
+++ app/back/socket/lakes/__init__.py
@@ -5,11 +5,11 @@ from back.mongo.data.collect.lakes.mongo import find_lake, find_lakes
 def connect_lakes(app):
 
     @app.on("get_lake")
-    def get_lake(id):
+    def get_lake(query={}, filter={"_id": 0}, detail="micro"):
 
-        emit("new_lake", find_lake({"id": id}))
+        emit("new_lake", find_lake(query, filter, detail))
 
     @app.on("get_lakes")
-    def get_lakes(limit):
+    def get_lakes(query={}, filter={"_id": 0}, sort=[("properties.id", 1)], detail="micro"):
 
-        emit("new_lakes", find_lakes(limit))
+        emit("new_lakes", find_lakes(query, filter, [tuple(item) for item in sort], detail))
