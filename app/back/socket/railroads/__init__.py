44f36275d6fba3e8b73813e510dda84daddaf015
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-18 04:03:51 +0300

    railroads socket update

--- app/back/socket/railroads/__init__.py
+++ app/back/socket/railroads/__init__.py
@@ -5,11 +5,11 @@ from back.mongo.data.collect.railroads.mongo import find_railroad, find_railroad
 def connect_railroads(app):
 
     @app.on("get_railroad")
-    def get_railroad(id):
+    def get_railroad(query={}, filter={"_id": 0}, detail="micro"):
 
-        emit("new_railroad", find_railroad({"id": id}))
+        emit("new_railroad", find_railroad(query, filter, detail))
 
     @app.on("get_railroads")
-    def get_railroads(limit):
+    def get_railroads(query={}, filter={"_id": 0}, sort=[("properties.id", 1)], detail="micro"):
 
-        emit("new_railroads", find_railroads(limit))
+        emit("new_railroads", find_railroads(query, filter, [tuple(item) for item in sort], detail))
