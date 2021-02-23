6ce8cc2ed57b49c0fafb0f11921a73b5fb9f60f0
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-24 00:06:13 +0300

    re-adding home route

---
+++ app/front/tree/home/route.py
@@ -0,0 +1,14 @@
+from flask import render_template, request
+
+from back.mongo.data.collect.clients import valid_client
+
+def register_home_route(app):
+
+    @app.route("/")
+    def home():
+
+        data = {"plot": {"type": None}}
+
+        if "id" in request.cookies: data["client"] = valid_client(request.cookies.get("id"))
+
+        return render_template("tree/home/page.html", data=data)
