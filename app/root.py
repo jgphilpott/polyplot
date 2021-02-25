2e1ef0ca4477a3c2e5c9898e6cd3be1a02588fd2
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-25 21:40:36 +0300

    root update

--- app/root.py
+++ app/root.py
@@ -4,11 +4,10 @@ from back.settings.setup import setup
 from back.settings.launch import launch
 
 name = "Polyplot"
+folder = "app/front"
 
-app_folder = "app/front"
+app = Flask(name, template_folder=folder, static_folder=folder)
 
-app = Flask(name, template_folder=app_folder, static_folder=app_folder)
-
-polyplot = setup(app, app_folder)
+polyplot = setup(app, folder)
 
 launch(polyplot)
