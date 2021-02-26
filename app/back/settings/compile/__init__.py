d38338ed2729d52713f880c53bc4746aace25f3c
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-26 17:31:38 +0300

    settings update

--- app/back/settings/compile/__init__.py
+++ app/back/settings/compile/__init__.py
@@ -1,8 +1,11 @@
+import subprocess
 from sass import compile
 
 def compile_all(path):
 
     compile_sass(path)
+
+    subprocess.Popen(["boussole", "watch"], cwd="app/back/settings/compile")
 
 def compile_sass(path):
 
