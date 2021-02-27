324a16ebbdd7307f2efd71f095aac0cc5251f387
Jacob Philpott <jacob.philpott@gmx.com>
2021-02-27 21:57:10 +0300

    adding threads to mongo data loader

--- app/back/mongo/backups/loader.py
+++ app/back/mongo/backups/loader.py
@@ -1,9 +1,10 @@
+from threading import Thread
 from back.mongo.backups.js import load_js
 from back.mongo.backups.json import load_json
 from back.mongo.backups.sass import load_sass
 
 def load_data(path):
 
-    load_js(path)
-    load_json(path)
-    load_sass(path)
+    Thread(target=load_js, args=(path,)).start()
+    Thread(target=load_json, args=(path,)).start()
+    Thread(target=load_sass, args=(path,)).start()
