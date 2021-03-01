617c82280eeabab729903fa810d9e47d5be91776
Jacob Philpott <jacob.philpott@gmx.com>
2021-03-02 01:59:38 +0300

    panels update

---
+++ app/front/libs/mjs/panels/all.mjs
@@ -0,0 +1,44 @@
+import {addCountriesPanel} from "./countries.mjs"
+import {addIndicatorsPanel} from "./indicators.mjs"
+import {addLayersPanel} from "./layers.mjs"
+import {addLegendPanel} from "./legend.mjs"
+import {addLinePanel} from "./line.mjs"
+import {addMapPanel} from "./map.mjs"
+import {addMetaPanel} from "./meta.mjs"
+import {addTimePanel} from "./time.mjs"
+import {addTitlePanel} from "./title.mjs"
+
+export function addPanels() {
+
+  let panelSettings = localRead("settings").panels
+
+  addIndicatorsPanel(panelSettings.indicators)
+  addLegendPanel(panelSettings.legend)
+  addMetaPanel(panelSettings.meta)
+  addTimePanel(panelSettings.time)
+  addTitlePanel(panelSettings.title)
+
+  if (data.plot.type == "Map") {
+
+    addLayersPanel(panelSettings.layers)
+    addLinePanel(panelSettings.line)
+
+  } else if (data.plot.type == "Poly2") {
+
+    addCountriesPanel(panelSettings.countries)
+    addMapPanel(panelSettings.map)
+
+  } else if (data.plot.type == "Poly3") {
+
+    addCountriesPanel(panelSettings.countries)
+    addMapPanel(panelSettings.map)
+
+  }
+
+  $("#canvas, .panel").mouseover(function() {
+
+    $("#context-menu").remove()
+
+  })
+
+}
