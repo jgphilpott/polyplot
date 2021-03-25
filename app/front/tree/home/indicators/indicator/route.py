56e01f0b7bc0e84c41b28eb02ddd871d8b3d6831
Jacob Philpott <jacob.philpott@gmx.com>
2021-03-25 18:36:36 +0300

    routes update

---
+++ app/front/tree/home/indicators/indicator/route.py
@@ -0,0 +1,31 @@
+from flask import request, make_response, render_template, abort
+
+from back.mongo.data.collect.clients import valid_client
+from back.mongo.data.collect.indicators import find_indicator
+
+def register_indicator_route(app):
+
+    @app.route("/indicators/<indicator>")
+    def indicator(indicator):
+
+        try:
+
+            data = {"plot": {"plots": find_indicator({"code": indicator}), "type": "Indicator"}}
+
+            if "id" in request.cookies: data["client"] = valid_client(request.cookies.get("id"))
+
+            min_cap = request.args.get("minCap") if "minCap" in request.args else request.cookies.get("minCap") if "minCap" in request.cookies else data["plot"]["plots"]["min_year"]
+            year = request.args.get("year") if "year" in request.args else request.cookies.get("year") if "year" in request.cookies else round((data["plot"]["plots"]["min_year"] + data["plot"]["plots"]["max_year"]) / 2)
+            max_cap = request.args.get("maxCap") if "maxCap" in request.args else request.cookies.get("maxCap") if "maxCap" in request.cookies else data["plot"]["plots"]["max_year"]
+
+            response = make_response(render_template("tree/home/indicators/indicator/page.html", data=data))
+
+            response.set_cookie("minCap", str(min_cap))
+            response.set_cookie("year", str(year))
+            response.set_cookie("maxCap", str(max_cap))
+
+            return response
+
+        except:
+
+            abort(404)
