1fd55d749256714f16ff7c6f502a3ac2452bde18
Jacob Philpott <jacob.philpott@gmx.com>
2021-04-11 10:14:01 +0300

    scales update

---
+++ app/front/libs/mjs/scales/domain.mjs
@@ -0,0 +1,24 @@
+let plot = data.plot
+
+export function findDomain(axis, plots=plot.plots) {
+
+  let domain = []
+
+  for (let i = 0; i < plots.length; i++) {
+
+    for (let j = 0; j < plots[i][axis].length; j++) {
+
+      let year = plots[i][axis][j].year
+      let value = plots[i][axis][j].value
+
+      if (year >= plot.t.minCap && year <= plot.t.maxCap && typeof(value) == "number") {
+        domain.push(value)
+      }
+
+    }
+
+  }
+
+  return [minValue(domain), maxValue(domain)]
+
+}
---
+++ app/front/libs/mjs/scales/range.mjs
@@ -0,0 +1,10 @@
+export let min = 0
+export let max = 100
+
+export function width() {
+  return $(window).width()
+}
+
+export function height() {
+  return $(window).height()
+}
