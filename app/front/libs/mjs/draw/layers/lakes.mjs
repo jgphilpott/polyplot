efb9fc352dffe10a1bd2319285342ab43cf303cf
Jacob Philpott <jacob.philpott@gmx.com>
2021-04-16 00:22:55 +0300

    layers update

---
+++ app/front/libs/mjs/draw/layers/lakes.mjs
@@ -0,0 +1,19 @@
+let plot = data.plot
+
+export function drawLakes(canvas, lakes=plot.GeoJSON.properties.layers.lakes) {
+
+  let geoProperties = plot.GeoJSON.properties
+  let checkpoint = geoProperties.layers.checkpoint
+
+  $(".lake").remove()
+
+  canvas.selectAll(".lake")
+        .data(lakes.filter(function(lake) { return lake.properties.rank <= checkpoint }))
+        .enter()
+        .append("path")
+        .attr("d", d3.geoPath().projection(geoProperties.projection))
+        .attr("class", "lake")
+        .attr("transform", geoProperties.zoom)
+        .style("fill", "lightblue")
+
+}
