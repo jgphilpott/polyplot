61ad9ce5bee91d13581fa8ee26ee94e0a852bd32
Jacob Philpott <jacob.philpott@gmx.com>
2021-05-02 00:46:34 -0400

    zoom update

--- app/front/libs/mjs/cartography/zoom.mjs
+++ app/front/libs/mjs/cartography/zoom.mjs
@@ -6,24 +6,30 @@ export function makeZoomable() {
   let zoom = d3.zoom()
                .scaleExtent([1, 64])
                .translateExtent([[0, 0], [width(), height()]])
-               .on("zoom", function(event) {
-
-                 updateLayers(d3.event.transform.k)
+               .on("zoom", function(event) { transform(d3.event.transform) })
+               .on("end", function(event) {
 
                  data.plot.GeoJSON.properties.zoom = d3.event.transform
 
-                 d3.selectAll(".airport").attr("transform", d3.event.transform)
-                 d3.selectAll(".city").attr("transform", d3.event.transform)
-                 d3.selectAll(".graticule").attr("transform", d3.event.transform)
-                 d3.selectAll(".lake").attr("transform", d3.event.transform)
-                 d3.selectAll(".map").attr("transform", d3.event.transform)
-                 d3.selectAll(".port").attr("transform", d3.event.transform)
-                 d3.selectAll(".railroad").attr("transform", d3.event.transform)
-                 d3.selectAll(".river").attr("transform", d3.event.transform)
-                 d3.selectAll(".road").attr("transform", d3.event.transform)
+                 updateLayers(d3.event.transform.k)
+                 transform(d3.event.transform)
 
                })
 
   d3.select("#canvas").call(zoom)
+
+}
+
+function transform(event) {
+
+  d3.selectAll(".airport").attr("transform", event)
+  d3.selectAll(".city").attr("transform", event)
+  d3.selectAll(".graticule").attr("transform", event)
+  d3.selectAll(".lake").attr("transform", event)
+  d3.selectAll(".map").attr("transform", event)
+  d3.selectAll(".port").attr("transform", event)
+  d3.selectAll(".railroad").attr("transform", event)
+  d3.selectAll(".river").attr("transform", event)
+  d3.selectAll(".road").attr("transform", event)
 
 }
