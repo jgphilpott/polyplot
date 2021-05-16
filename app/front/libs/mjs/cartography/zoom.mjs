a5f4f99099fa6531c3048b61ad33106a9a62c697
Jacob Philpott <jacob.philpott@gmx.com>
2021-05-16 12:49:06 -0400

    cartography zoom update

--- app/front/libs/mjs/cartography/zoom.mjs
+++ app/front/libs/mjs/cartography/zoom.mjs
@@ -1,34 +1,37 @@
-import {updateLayers} from "../draw/layers/all.mjs"
 import {width, height} from "../env/window.mjs"
 
+import {updateLayers} from "../draw/layers/all.mjs"
+import {updateSettings} from "../panels/menu.mjs"
+
 export function makeZoomable(canvas) {
+
+  let zoomSpeed = data.client.settings.map.zoomSpeed
 
   let zoom = d3.zoom()
                .scaleExtent([1, 64])
                .translateExtent([[0, 0], [width(), height()]])
-               .on("zoom", function(event) { transform(d3.event.transform) })
+               .on("zoom", function(event) {
+
+                 d3.selectAll(".airport").attr("transform", d3.event.transform)
+                 d3.selectAll(".city").attr("transform", d3.event.transform)
+                 d3.selectAll(".graticule").attr("transform", d3.event.transform)
+                 d3.selectAll(".lake").attr("transform", d3.event.transform)
+                 d3.selectAll(".map").attr("transform", d3.event.transform)
+                 d3.selectAll(".port").attr("transform", d3.event.transform)
+                 d3.selectAll(".railroad").attr("transform", d3.event.transform)
+                 d3.selectAll(".river").attr("transform", d3.event.transform)
+                 d3.selectAll(".road").attr("transform", d3.event.transform)
+
+               })
                .on("end", function(event) {
 
-                 data.plot.GeoJSON.properties.zoom = d3.event.transform
+                 data.plot.GeoJSON.properties.transform = d3.event.transform
 
+                 updateSettings("map", "transform", d3.event.transform)
                  updateLayers("zoom", d3.event.transform.k)
 
                })
 
   canvas.call(zoom)
-
-}
-
-function transform(event) {
-
-  d3.selectAll(".airport").attr("transform", event)
-  d3.selectAll(".city").attr("transform", event)
-  d3.selectAll(".graticule").attr("transform", event)
-  d3.selectAll(".lake").attr("transform", event)
-  d3.selectAll(".map").attr("transform", event)
-  d3.selectAll(".port").attr("transform", event)
-  d3.selectAll(".railroad").attr("transform", event)
-  d3.selectAll(".river").attr("transform", event)
-  d3.selectAll(".road").attr("transform", event)
 
 }
