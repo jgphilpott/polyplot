26c6459a18fc18986797dd3a8b106c6fe5d9f68f
Jacob Philpott <jacob.philpott@gmx.com>
2021-05-18 20:12:15 -0400

    cartography polymorph update

--- app/front/libs/mjs/cartography/projections.mjs
+++ app/front/libs/mjs/cartography/projections.mjs
@@ -1,6 +1,7 @@
 // See all map projections here: github.com/d3/d3-geo-projection/blob/master/README.md
 
 import {width, height} from "../env/window.mjs"
+import {translateLayers} from "./translate.mjs"
 
 export let equirectangular = d3.geoEquirectangular()
 export let mercator = d3.geoMercator()
@@ -32,70 +33,69 @@ export function isProjected(feature, projection) {
 
 }
 
-// export function polymorph(projectionName, duration=1000) {
-//
-//   let steps = 5
-//   let progress = 0
-//
-//   let mapProperties = data.plot.GeoJSON.properties
-//   let mapSettings = data.client.settings.map
-//
-//   let orientation = mapSettings.orientation
-//
-//   let projection = newProjection(projectionName)
-//
-//   let projectionOne = mapProperties.projection
-//   let projectionTwo = projection
-//
-//   let morphInterval = setInterval(function() {
-//
-//     duration ? progress += 1 / steps : progress += 1
-//
-//     if (progress <= 1) {
-//       // console.log("morph");
-//       d3.select("#canvas").selectAll("path").attr("d", morph)
-//       // d3.select("#canvas").selectAll(".airport").attr("d", console.log(morph))
-//       // console.log(morph([]));
-//     } else {
-//       clearInterval(morphInterval)
-//       mapProperties.projection = projection
-//       $(".graticule").css("visibility", "visible")
-//     }
-//
-//   }, duration / steps)
-//
-//   function morph(coordinate) {
-//
-//     let clip = Math.PI
-//     let extent = projectionName == "orthographic" ? 0.95 : 1
-//
-//     let morphProjection = d3.geoProjection(project)
-//                             .fitExtent([[0, 0], [width() * extent, height() * extent]], {type: "Sphere"})
-//                             .clipExtent([[0, 0], [width(), height()]])
-//                             .translate([width() / 2, height() / 2])
-//                             .preclip(function(preclip) { return d3.geoClipCircle(clip)(d3.geoClipAntimeridian(preclip)) })
-//                             .rotate([orientation.λ, orientation.φ, orientation.γ])
-//
-//     function project(λ, φ) {
-//
-//       λ *= 180 / Math.PI
-//       φ *= 180 / Math.PI
-//
-//       let coordinateOne = projectionOne([λ, φ])
-//       let coordinateTwo = projectionTwo([λ, φ])
-//
-//       clip = projectionName == "orthographic" ? Math.PI/2 + (1 - progress) * Math.PI/2 : Math.PI/2 + progress * Math.PI/2
-//
-//       return [(1 - progress) * coordinateOne[0] + progress * coordinateTwo[0], (1 - progress) * -coordinateOne[1] + progress * -coordinateTwo[1]]
-//
-//     }
-//
-//     let morphPath = d3.geoPath(morphProjection)
-//
-//     // mapProperties.projection = morphProjection
-//
-//     return morphPath(coordinate)
-//
-//   }
-//
-// }
+export function polymorph(name, duration=1000) {
+
+  let steps = 10
+  let progress = 0
+
+  let mapProperties = data.plot.GeoJSON.properties
+  let mapSettings = data.client.settings.map
+
+  let projectionOne = mapProperties.projection
+  let projectionTwo = newProjection(name)
+
+  let morphInterval = setInterval(function() {
+
+    duration ? progress += 1 / steps : progress += 1
+
+    if (progress <= 1) {
+
+      translateLayers(morph())
+
+    } else {
+
+      clearInterval(morphInterval)
+
+      mapProperties.projection = projectionTwo
+
+    }
+
+  }, duration / steps)
+
+  function morph(coordinate) {
+
+    let clip = Math.PI
+    let extent = name == "orthographic" ? 0.95 : 1
+
+    let orientation = mapSettings.orientation
+
+    let λ = orientation.λ
+    let φ = orientation.φ
+    let γ = orientation.γ
+
+    let morphProjection = d3.geoProjection(project)
+                            .fitExtent([[0, 0], [width() * extent, height() * extent]], {type: "Sphere"})
+                            .preclip(function(preclip) { return d3.geoClipCircle(clip)(d3.geoClipAntimeridian(preclip)) })
+                            .clipExtent([[0, 0], [width(), height()]])
+                            .translate([width() / 2, height() / 2])
+                            .rotate([λ, φ, γ])
+
+    function project(λ, φ) {
+
+      λ *= 180 / Math.PI
+      φ *= 180 / Math.PI
+
+      let coordinateOne = projectionOne([λ, φ])
+      let coordinateTwo = projectionTwo([λ, φ])
+
+      clip = name == "orthographic" ? Math.PI/2 + (1 - progress) * Math.PI/2 : Math.PI/2 + progress * Math.PI/2
+
+      return [(1 - progress) * coordinateOne[0] + progress * coordinateTwo[0], (1 - progress) * -coordinateOne[1] + progress * -coordinateTwo[1]]
+
+    }
+
+    return morphProjection
+
+  }
+
+}
